dc6efdda11665222c619d016a5505bda8ff01404
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-09 20:36:21 +0000

    upadates 2025-01-9 17:30h

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-echo "Hello World!"
-?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>AMM001</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="">
+</head>
+
+<body>
+Teste 001!
+</body>
+</html>
