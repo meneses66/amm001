f9aa9528c53ac1e001274456ca1b53dd41c9f1c8
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-15 20:14:07 +0000

    20250115 1700h - fixes

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
 	<title>AMM001</title>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width-device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/estyles.css">
 </head>
 
