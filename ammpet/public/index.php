d032f9b0249ae550b0117b82602113a2b0421771
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-16 19:26:12 +0000

    20250116 - updates

--- ammpet/public/index.php
+++ ammpet/public/index.php
@@ -2,7 +2,32 @@
 	session_start();
 	require_once('../classes/core/config.php');
 
-	echo "This is Index.php";
+	#echo "This is Index.php";
 
 	#$app = new App;
 	#$app->loadController();
+
+	function splitURL()
+    {
+        $URL = $_GET['url'] ?? 'home';
+        $URL = explode("/", $URL);
+        return $URL;
+    }
+
+    function loadController()
+    {
+        $URL = this->splitURL();
+        $fileName = "../classes/controllers/".ucfirst($URL[0]).".php";
+        if(file_exists($fileName))
+        {
+            #require $fileName;
+            print_r ($fileName);
+
+        } else{
+            $fileName = "../classes/controllers/_404.php";
+            #require $fileName;
+            print_r ($fileName);
+        }
+    }
+
+	loadController();
