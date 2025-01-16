3fd0ca475f2f79ea08fef947a4e3cc552199568b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-16 18:53:52 +0000

    20250116 - updates

--- ammpet/public/index.php
+++ ammpet/public/index.php
@@ -3,5 +3,14 @@
 	require_once('../classes/core/config.php');
 
 	echo "This is Index.php";
+
+	function show($anything){
+		echo "<pre>";
+		print_r($anything);
+		echo "</pre>";
+	}
+	
+	show($_GET);
+
 #	$app = new App;
 #	$app->loadController();
