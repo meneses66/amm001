6e91197f052a8ddde0403ba37fb69d667d45464e
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-17 18:26:05 +0000

    20250117 - updates

--- ammpet/classes/core/init.php
+++ ammpet/classes/core/init.php
@@ -1,9 +1,9 @@
 <?php
 
-require 'config.php';
 require 'functions.php';
 require 'properties.php';
 require '_DAO.php';
 require '_GlobalModel.php';
 require '_GlobalController.php';
+require 'config.php';
 require 'App.php';
