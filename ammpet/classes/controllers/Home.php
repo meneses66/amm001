df2b56c1d1a6c18fae9fc0dd259af49572fd8b05
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-17 18:11:22 +0000

    20250117 - updates

--- ammpet/classes/controllers/Home.php
+++ ammpet/classes/controllers/Home.php
@@ -9,10 +9,9 @@ class Home extends _GlobalController {
         $array[ID]='1';
         $array2[NAME]='Joao';
         $all_arrays = array_merge($array, $array2);
-        show($all_arrays);
-        #$model = new _GlobalModel;
-        #$result = $model->list($array,$array2);
-        #show($result);
+        $model = new _GlobalModel;
+        $result = $model->list($array,$array2);
+        show($result);
 
         $this->view('home');
     }
