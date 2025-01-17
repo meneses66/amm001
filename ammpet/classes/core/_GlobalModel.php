19eaa69bbd31ac12f59102a62724a3d7148c49a1
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-17 18:12:20 +0000

    20250117 - updates

--- ammpet/classes/core/_GlobalModel.php
+++ ammpet/classes/core/_GlobalModel.php
@@ -23,9 +23,9 @@ class _GlobalModel{
 
         $all_inputs =array_merge($inputs, $inputs_not);
 
-        #return $this->query($sql_stm, $all_inputs);
+        return $this->query($sql_stm, $all_inputs);
         
-        echo $sql_stm;
+        #echo $sql_stm;
 
     }
     #getRow
