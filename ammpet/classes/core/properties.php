04afffe50a7d0c937c65734c097524ee6e4b7926
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-17 18:03:02 +0000

    20250117 - updates

--- ammpet/classes/core/properties.php
+++ ammpet/classes/core/properties.php
@@ -1,5 +1,8 @@
 <?php
-
+    define('SERVERNAME','node213878-amm01.sp1.br.saveincloud.net.br:3306/');
+    define('DBNAME','dbpetshop');
+    define('DBUSER','ammphp');
+    define('DBPWD','Carol@21102012');
 if($_SERVER['SERVER_NAME'] == 'localhost'){
     define('ROOT','http://localhost/ammpet/public');
     define('SERVERNAME','localhost:3306/');
