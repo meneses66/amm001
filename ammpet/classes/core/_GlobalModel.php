503a3f33133ee74e5fd92dde0b008425ed98de1d
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-21 13:47:34 +0000

    20250121 - updates

--- ammpet/classes/core/_GlobalModel.php
+++ ammpet/classes/core/_GlobalModel.php
@@ -83,7 +83,7 @@ class _GlobalModel{
     {
         $inputs['ID']=$ID;
         $keys = array_keys($inputs);
-        $sql_stm = "delete $this->table where ID=:ID";
+        $sql_stm = "delete from $this->table where ID = :ID";
 
         $this->query($sql_stm);
         return false;
