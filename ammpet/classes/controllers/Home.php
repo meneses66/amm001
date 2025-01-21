e7bf39ea7aebc2df9ceaffe1ccd4e1af43a791fb
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-21 18:46:42 +0000

    20250121 - updates

--- ammpet/classes/controllers/Home.php
+++ ammpet/classes/controllers/Home.php
@@ -3,9 +3,13 @@
 class Home {
     
     use _GlobalModel;
+    
     public function index()
     {
         echo "This is home controller";
+        $supplier = new Supplier;
+        $result = $this-> $supplier->listAll();
+        return $result;
         $this->view('home');
     }
 }
--- ammpet/classes/controllers/Supplier.php
+++ ammpet/classes/controllers/Supplier.php
@@ -4,17 +4,11 @@ class Supplier {
 
     use _GlobalModel;
     protected $table = 'SUPPLIER';
+    
     public function index()
     {
         echo "This is supplier controller";
     }
 
-    public function listSupplier()
-    {
-        return $this->listAll();   
-    }
-
+    
 }
-
-$listofsuppliers = $this->listSupplier();
-return $listofsuppliers;
