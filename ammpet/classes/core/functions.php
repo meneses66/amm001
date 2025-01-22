f1b3f76a39cdf3d42297892af0136bae488308c1
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-01-22 21:27:02 +0000

    20250122 - updates

--- ammpet/classes/core/functions.php
+++ ammpet/classes/core/functions.php
@@ -2,6 +2,36 @@
 
 defined('ROOTPATH') OR exit('Access denied!');
 
+function URL($key):mixed
+{
+	$URL = $_GET['url'] ?? 'home';
+	$URL = explode("/", trim($URL,"/"));
+	
+	switch ($key) {
+		case 'page':
+		case 0:
+			return $URL[0] ?? null;
+			break;
+		case 'section':
+		case 'slug':
+		case 1:
+			return $URL[1] ?? null;
+			break;
+		case 'action':
+		case 2:
+			return $URL[2] ?? null;
+			break;
+		case 'id':
+		case 3:
+			return $URL[3] ?? null;
+			break;
+		default:
+			return null;
+			break;
+	}
+
+}
+
 function show($anything){
     echo "<pre>";
     print_r($anything);
@@ -10,4 +40,31 @@ function show($anything){
 
 function esc($str){
     return htmlspecialchars($str);
+}
+
+function redirect($path){
+    header("Location: " . ROOT."/".$path);
+    die;
+}
+
+/** Displays message to the user */
+function message(string $msg=null, bool $clear=false){
+    $ses = new Core\Session();
+    if(!empty($msg))
+    {
+        $ses->set('message', $msg);
+    } else if(!empty($ses->get('message')))
+            {
+            
+                $msg = $ses->get('message');
+            
+                if($clear)
+                {
+                
+                    $ses->pop('message');
+                }
+                return $msg;
+            }
+            return false;
+    
 }
--- ammpet/public/index.php
+++ ammpet/public/index.php
@@ -1,6 +1,13 @@
 <?php
 	session_start();
 
+	/** Check for valid PHP version */
+	$minPHPVersion = '8.0';
+	if(phpversion() < $minPHPVersion)
+	{
+		die("Your PHP version must be {$minPHPVersion} or higher to run this app. Your current PHP vresion is ". phpversion());
+	}
+
 	/** Path to this file */
 	define('ROOTPATH', __DIR__ . DIRECTORY_SEPARATOR);
 	require '../classes/core/init.php';
