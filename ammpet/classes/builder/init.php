f03bffc5cd71896ac399af610ef927100051987f
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-11 12:17:51 +0000

    2025-02-11 - updates

--- ammpet/classes/builder/init.php
+++ ammpet/classes/builder/init.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once 'classes'.DS.'core'.DS.'properties.php';
 require_once 'classes'.DS.'core'.DS.'config.php';
 require_once 'classes'.DS.'core'.DS.'_DAO.php';
 require_once '_DAO.php';
