bf35ab69160cc750ebdf3830604d6129231407d6
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-11 11:46:18 +0000

    20250211 - updates

--- ammpet/classes/core/properties.php
+++ ammpet/classes/core/properties.php
@@ -2,8 +2,14 @@
 
 defined('ROOTPATH') OR exit('Access denied!');
 
-/** if((empty($_SERVER['SERVER_NAME']) && php_sapi_name() == 'cli') || (!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost')) */
-if((!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost'))
+if((empty($_SERVER['SERVER_NAME']) && php_sapi_name() == 'cli'))
+{
+    define('ROOT','https://amm01.sp1.br.saveincloud.net.br/ammpet/public');
+    define('SERVERNAME','node213878-amm01.sp1.br.saveincloud.net.br:3306/');
+    define('DBNAME','dbpetshop');
+    define('DBUSER','ammphp');
+    define('DBPWD','Carol@21102012');
+} else if((!empty($_SERVER['SERVER_NAME']) && $_SERVER['SERVER_NAME'] == 'localhost'))
 {
     define('ROOT','http://localhost/ammpet/public');
     define('SERVERNAME','localhost:3306/');
