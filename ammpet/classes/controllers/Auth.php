7052df6a7304058e36a48c259975e452f5de611a
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-17 18:27:42 +0000

    20250217 - upd

--- ammpet/classes/controllers/Auth.php
+++ ammpet/classes/controllers/Auth.php
@@ -7,15 +7,3 @@
 
     $init = new \Controller\Login();
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
-        $login = $_POST['login'];
-        $pass = $_POST['pass'];
-        $operation = $_POST['op'];
-        switch($operation)
-        {
-            case signin:
-            $init->signin($login,$pass) ? $init->login() : $init->logout();
-            break;
-        }
-        
-    }
