f5efe82ba0ada2310a3577702457172305c525b0
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-17 18:12:31 +0000

    20250217 - updates

--- ammpet/classes/controllers/Auth.php
+++ ammpet/classes/controllers/Auth.php
@@ -5,13 +5,31 @@
     defined('ROOTPATH');
     //OR exit('Access denied!');
 
-    $init = new Auth();
-    $init->Index();
+    $int = new Auth();
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $login = $_POST['login'];
+        $pass = $_POST['pass'];
+        $operation = $_POST['op'];
+        switch($operation)
+        {
+            case signin:
+            return $init->login();
+            break;
+
+        }
+        
+    }
 
     class Auth{
 
         public function Index(){
             return "This is Auth Controller";
+        }
+
+        public function login()
+        {
+            $this->view('login/login2');
         }
 
     }
