801de82b64c6ce47e0e9d9d71156b5f908f01ea9
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-18 21:45:22 +0000

    20250218

--- ammpet/classes/controllers/Home.php
+++ ammpet/classes/controllers/Home.php
@@ -10,7 +10,7 @@ class Home {
     
     public function index()
     {
-        echo "This is home controller. PHP info is: ";
+        echo "This is home controller.";
         
         $this->view('home/home');
     }
--- ammpet/classes/views/home/home.view.php
+++ ammpet/classes/views/home/home.view.php
@@ -1,4 +1,22 @@
-<h1>Home page View</h1>
-<?php
-echo ROOT;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+    <!-- Side navigation -->
+    <div class="sidenav">
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+    </div>
+
+    <!-- Page content -->
+    <div class="main">
+    ...
+    </div>
+</body>
+</html>
+
+
