ed3ce07707a019eda646b3d8241020e795a2f56f
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-19 14:18:46 +0000

    20250219

--- ammpet/classes/views/home/home.view.php
+++ ammpet/classes/views/home/home.view.php
@@ -9,7 +9,7 @@
 
   <div class="container">
     <div class="header"><h2>My Header</h2></div>
-    <div class="menu"><?php include ROOT_CLASSES . '/views/sidebar.view.php';?></div>
+    <div class="menu"><?php include ROOT . '/../views/sidebar.view.php';?></div>
     <div class="content"><h3><?php include "main.view.php";?></h3></div>
     <div class="footer"><h4>Footer</h4></div>
   </div>
