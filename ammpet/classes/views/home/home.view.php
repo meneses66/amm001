398bf2b8230997ebcdf11853fff268b29c3bca49
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-19 13:53:07 +0000

    20250219

--- ammpet/classes/views/home/home.view.php
+++ ammpet/classes/views/home/home.view.php
@@ -5,12 +5,14 @@
 </head>
 <body>
 
-<div class="container">
-  <div class="header"><h2>My Header</h2></div>
-  <div class="menu"><?php include "../views/sidebar.view.php";?></div>
-  <div class="content"><h3><?php include "main.view.php";?></h3></div>
-  <div class="footer"><h4>Footer</h4></div>
-</div>
+<link rel="stylesheet" href="../public/assets/css/styles.css">
+
+  <div class="container">
+    <div class="header"><h2>My Header</h2></div>
+    <div class="menu"><?php include "./views/sidebar.view.php";?></div>
+    <div class="content"><h3><?php include "main.view.php";?></h3></div>
+    <div class="footer"><h4>Footer</h4></div>
+  </div>
 
 </body>
 </html>
