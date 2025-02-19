aaf52f776d1bb553ee45a30cf300443ab3754a9b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-19 15:12:26 +0000

    20250219

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,5 +1,7 @@
 <div class="menu">
-    <input id="button" type="submit" value="Action A"><br><br>
+    <div><input id="button" type="submit" value="Action A"><input id="button" type="submit" value="Action E"></div><br><br>
     <input id="button" type="submit" value="Action B"><br><br>
     <input id="button" type="submit" value="Action C"><br><br>
+    <input id="button" type="submit" value="Action C"><br><br>
+    
 </div>
--- ammpet/classes/views/sidebar.view.php
+++ ammpet/classes/views/sidebar.view.php
@@ -1,11 +1,12 @@
    <!-- Sidebar  -->
    <nav id="sidebar">
-      <div class="sidebar-header">
-        <h3 class="tituloPanel"> Panel Administrador </h3>
-        <strong>PA</strong>
-      </div>
  
       <div class="btn-group">
+        <button class="button">Button</button>
+        <button class="button">Button</button>
+        <button class="button">Button</button>
+        <button class="button">Button</button>
+        <button class="button">Button</button>
         <button class="button">Button</button>
         <button class="button">Button</button>
         <button class="button">Button</button>
