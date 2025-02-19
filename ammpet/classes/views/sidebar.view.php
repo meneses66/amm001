f9b57ad71dd8e2d3124f771e265dab07896c7396
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-19 19:29:33 +0000

    20250219

--- ammpet/classes/views/sidebar.view.php
+++ ammpet/classes/views/sidebar.view.php
@@ -1,16 +1,9 @@
-   <!-- Sidebar  -->
-   <nav id="sidebar">
- 
-      <div class="btn-group">
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-        <button class="button">Button</button>
-      </div>
-
-  </nav>
+<div class="sidebar-heading border-bottom bg-light">Start Bootstrap</div>
+  <div class="list-group list-group-flush">
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Dashboard</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Shortcuts</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Overview</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Events</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Profile</a>
+      <a class="list-group-item list-group-item-action list-group-item-light p-3" href="#!">Status</a>
+  </div>
