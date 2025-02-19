bf76e99af57ddaf609f985066f0d9db26e41c3a5
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-19 15:13:43 +0000

    20250219

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,5 +1,5 @@
 <div class="menu">
-    <div><input id="button" type="submit" value="Action A"><input id="button" type="submit" value="Action E"></div><br><br>
+    <div><input id="button" type="submit" value="Action A">  <input id="button" type="submit" value="Action E"></div><br><br>
     <input id="button" type="submit" value="Action B"><br><br>
     <input id="button" type="submit" value="Action C"><br><br>
     <input id="button" type="submit" value="Action C"><br><br>
