9fd54c3eddd78199f19619f3dddf6d48306f41ae
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-20 22:13:02 +0000

    20250220

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="../public/assets/css/styles.css">
 <div class="container-fluid">
     <br><br><br>
     <div><input id="button" type="submit" value="Action A">  <input id="button" type="submit" value="Action B"></div><br><br><br>
