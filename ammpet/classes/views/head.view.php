9461b732b1ddf8ce30a7597c65f93c7d462b6414
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-20 22:03:27 +0000

    20250220

--- ammpet/classes/views/head.view.php
+++ ammpet/classes/views/head.view.php
@@ -4,6 +4,6 @@
 <meta name="author" content="" />
 <title>Simple Sidebar - Start Bootstrap Template</title>
 <!-- Favicon-->
-<link rel="icon" type="image/x-icon" href="../public/assets/images/favicon.ico" />
+<link rel="icon" type="image/x-icon" href="../public/assets/images/dog_icon.ico" />
 <!-- Core theme CSS (includes Bootstrap)-->
 <link href="../public/assets/css/styles_home.css" rel="stylesheet" />
