ca9a082b0e594042bcfaa1df1f3426156bdcbec1
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-21 01:01:28 +0000

    20250220

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,22 +1,34 @@
 <link rel="stylesheet" href="../public/assets/css/styles.css">
 <div class="container-fluid">
-    <br><br><br>
-            <form method="post" action="../public/Client">
-                <input type="hidden" name="op" value="list_customer">
-                <input id="button" type="submit" value="Clientes">
-            </form>
-            <br><br><br>
-            <form method="post" action="../public/OrderX">
-                <input type="hidden" name="op" value="list_orders">
-                <input id="button" type="submit" value="Vendas">
-            </form>
-            <br><br><br>
-            <form method="post" action="../public/Agenda">
-                <input type="hidden" name="op" value="agenda">
-                <input id="button" type="submit" value="Agenda">
-            </form>
-            <form method="post" action="../public/OrderX">
-                <input type="hidden" name="op" value="new_order">
-                <input id="button" type="submit" value="Nova Venda">
-            </form>
+
+    <table style="width:100%">
+        <tr>
+            <td>
+                <form method="post" action="../public/Client">
+                    <input type="hidden" name="op" value="list_customer">
+                    <input id="button" type="submit" value="Clientes">
+                </form>
+            </td>
+            <td>
+                <form method="post" action="../public/OrderX">
+                    <input type="hidden" name="op" value="list_orders">
+                    <input id="button" type="submit" value="Vendas">
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <form method="post" action="../public/Agenda">
+                    <input type="hidden" name="op" value="agenda">
+                    <input id="button" type="submit" value="Agenda">
+                </form>
+            </td>
+            <td>
+                <form method="post" action="../public/OrderX">
+                    <input type="hidden" name="op" value="new_order">
+                    <input id="button" type="submit" value="Nova Venda">
+                </form>
+            </td>
+        </tr>
+    </table>           
 </div>
