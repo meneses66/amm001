ee700b1dbf293384f61a4d4a60fde18daa4e1d5b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-21 00:53:46 +0000

    20250220

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,25 +1,32 @@
 <link rel="stylesheet" href="../public/assets/css/styles.css">
 <div class="container-fluid">
     <br><br><br>
-
     <div>
-        <form method="post" action="../public/Client">
-            <input type="hidden" name="op" value="list_customer">
-            <input id="button" type="submit" value="Clientes">
-        </form>
-        <form method="post" action="../public/OrderX">
-            <input type="hidden" name="op" value="list_orders">
-            <input id="button" type="submit" value="Vendas">
-        </form>
-    </div><br><br><br>
+        <div class="mycontainer">
+            <form method="post" action="../public/Client">
+                <input type="hidden" name="op" value="list_customer">
+                <input id="button" type="submit" value="Clientes">
+            </form>
+        </div>
+        <div class="mycontainer">
+            <form method="post" action="../public/OrderX">
+                <input type="hidden" name="op" value="list_orders">
+                <input id="button" type="submit" value="Vendas">
+            </form>
+        </div><br><br><br>
+    </div>
     <div>
-        <form method="post" action="../public/Agenda">
-            <input type="hidden" name="op" value="agenda">
-            <input id="button" type="submit" value="Agenda">
-        </form>
-        <form method="post" action="../public/OrderX">
-            <input type="hidden" name="op" value="new_order">
-            <input id="button" type="submit" value="Nova Venda">
-        </form>
+        <div class="mycontainer">
+            <form method="post" action="../public/Agenda">
+                <input type="hidden" name="op" value="agenda">
+                <input id="button" type="submit" value="Agenda">
+            </form>
+        </div>    
+        <div class="mycontainer">
+            <form method="post" action="../public/OrderX">
+                <input type="hidden" name="op" value="new_order">
+                <input id="button" type="submit" value="Nova Venda">
+            </form>
+        </div>
     </div><br><br><br>
 </div>
