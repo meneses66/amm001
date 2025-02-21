ccc7f1b01485cff3efc90fbe62dce3227ad0a66c
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-21 17:56:08 +0000

    20250221

--- ammpet/classes/models/User.php
+++ ammpet/classes/models/User.php
@@ -4,9 +4,9 @@ namespace Model;
 
 defined('ROOTPATH') OR exit('Access denied!');
 
-class User{
+class User extends Model\Supplier {
 
-    use _GlobalModel;
+    //use _GlobalModel;
     private $username;
     protected $table = 'SUPPLIER';
     
@@ -20,24 +20,6 @@ class User{
 
     function set_username($username){
         $this->username = $username;
-    }
-
-    public function check_user_pass($inputs){
-        $keys = array_keys($inputs);
-        $sql_stm = "select * from $this->table where ";
-        
-        foreach ($keys as $key){
-            $sql_stm .= $key . "= :" . $key . " && ";
-        }
-
-        $sql_stm = trim($sql_stm," && ");
-
-        $result = $this->query($sql_stm, $inputs);
-
-        if ($result)
-            return $result[0];
-
-        return false;
     }
 
 }
