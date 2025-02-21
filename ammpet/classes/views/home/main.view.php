a97ef01f69a64fb06d581e9eea0b02c5c24c0e98
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-21 00:39:47 +0000

    20250220

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,6 +1,25 @@
 <link rel="stylesheet" href="../public/assets/css/styles.css">
 <div class="container-fluid">
     <br><br><br>
-    <div>    <input id="button" type="submit" value="Action A">    <input id="button" type="submit" value="Action B"></div><br><br><br>
-    <div>    <input id="button" type="submit" value="Action C">    <input id="button" type="submit" value="Action D"></div><br><br><br>
+
+    <div>
+        <form method="post" action="../public/Client">
+            <input type="hidden" name="op" value="list_customer">
+            <input id="button" type="submit" value="Clientes">
+        </form>
+        <form method="post" action="../public/OrderX">
+            <input type="hidden" name="op" value="list_orders">
+            <input id="button" type="submit" value="Vendas">
+        </form>
+    </div><br><br><br>
+    <div>
+        <form method="post" action="../public/Agenda">
+            <input type="hidden" name="op" value="agenda">
+            <input id="button" type="submit" value="Agenda">
+        </form>
+        <form method="post" action="../public/OrderX">
+            <input type="hidden" name="op" value="new_order">
+            <input id="button" type="submit" value="Nova Venda">
+        </form>
+    </div><br><br><br>
 </div>
