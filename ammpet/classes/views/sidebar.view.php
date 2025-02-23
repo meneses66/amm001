9a7e665a43eeda554a6635f73376b21cd64ac1b4
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-23 15:41:57 +0000

    20250223

--- ammpet/classes/views/sidebar.view.php
+++ ammpet/classes/views/sidebar.view.php
@@ -2,12 +2,12 @@
   <div class="list-group list-group-flush">
       <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Home' && URL_1=="") echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Home";?>">Home</a>
       <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='create') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Home/create";?>">Cadastros</a>
-      <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='search') echo 'active';}?> list-group-item-light p-3" href="?run=search">Consultas</a>
-      <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='report') echo 'active';}?> list-group-item-light p-3" href="?run=report">Relatórios</a>
-      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='OrderX') echo 'active';}?> list-group-item-light p-3" href="?run=orderx">Vendas</a>
-      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Agenda') echo 'active';}?> list-group-item-light p-3" href="?run=agenda">Agenda</a>
-      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Control') echo 'active';}?> list-group-item-light p-3" href="?run=control">Caixa</a>
-      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Admin') echo 'active';}?> list-group-item-light p-3" href="?run=admin">Admin</a>
+      <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='search') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Home/search";?>">Consultas</a>
+      <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='report') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Home/report";?>">Relatórios</a>
+      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='OrderX') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/OrderX";?>">Vendas</a>
+      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Agenda') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Agenda";?>">Agenda</a>
+      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Control') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Control";?>">Caixa</a>
+      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Admin') echo 'active';}?> list-group-item-light p-3" href="<?php echo ROOT."/Admin";?>">Admin</a>
   </div>
 
 <?php
