7d4016d54bf67d02da03b58910186c9855df6b56
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-23 15:21:16 +0000

    20250223

--- ammpet/classes/views/topnav.view.php
+++ ammpet/classes/views/topnav.view.php
@@ -4,7 +4,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                <li class="nav-item"><a class="nav-link"><?php echo "Usuário: ".$_SESSION['username'];?></a></li></li>
+                <li class="nav-item"><a class="nav-link"><?php if(!isset($_SESSION['username'])) {session_start();} echo "Usuário: ".$_SESSION['username'];?></a></li></li>
                 <li class="nav-item active"><a class="nav-link" href="?run=logout">Sair</a></li>
             </ul>
         </div>
