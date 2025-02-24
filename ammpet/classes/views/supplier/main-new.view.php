60e407f24aae200c03768318739806fc27daf4bd
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-24 23:08:36 +0000

    20250224

--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -7,6 +7,12 @@
             <div>
                 <label for="name" class="medium-label">Nome: </label><input id="name" type="text" size="50" name="name"><br><br>
                 <label for="xyz" class="medium-label">XYZ: </label><input id="xyz" type="text" size="30" name="xyz"><br><br>
+                <label for="qwe" class="medium-label">QWE: </label>
+                <select id="qwe" type="text" size="20" name="qwe">
+                    <option value="abc">ABC</option>
+                    <option value="zxc">ZXC</option>
+                    <option value="wer">WER</option>
+                </select><br><br>
             </div>
             <div>
                 <label for="type" class="medium-label">Tipo: </label><input id="type" type="text" size="20" name="type"><br><br>
