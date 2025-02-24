c0efa8a8d74abb6c9eb9fc1007424624c1a9e749
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-24 18:18:57 +0000

    20250224

--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -1,23 +1,23 @@
 <link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css">
     <div>Novo Fornecedor</div>
     <br><br>
-    <div>
-        <form method="post" action="../public/Supplier">
-            <input type="hidden" name="op" value="insert">	
-            <div class="split left">
-                <div class="left">
-                    <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: black;">Nome:</div>
-                    <input id="name" type="text" name="name"><br><br>
-                </div>
+    <form method="post" action="../public/Supplier">
+    <div>   
+        <input type="hidden" name="op" value="insert">	
+        <div class="split left">
+            <div class="left">
+                <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: black;">Nome:</div>
+                <input id="name" type="text" name="name"><br><br>
             </div>
+        </div>
 
-            <div class="split right">
-                <div class="left">
-                    <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: black;">Data:</div>
-                    <input id="date" type="date" name="date"><br><br><br>
+        <div class="split right">
+            <div class="left">
+                <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: black;">Data:</div>
+                <input id="date" type="date" name="date"><br><br><br>
 
-                    <input id="button" type="submit" value="Criar"><br><br>
-                </div>
+                <input id="button" type="submit" value="Criar"><br><br>
             </div>
-        </form>
+        </div>
     </div>
+</form>
