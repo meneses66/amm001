ead5bdbeea80c06bc6fffa6fe376d4887f14e9d2
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-24 19:26:36 +0000

    20250224

--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -5,7 +5,7 @@
     <input type="hidden" name="op" value="insert">
         <div class="flex-container">
             <div>
-                <label for="name" font-size="12px">Nome: </label><input id="name" type="text" size="50" name="name"><br><br>
+                <label for="name" font-size:"12px">Nome: </label><input id="name" type="text" size="50" name="name"><br><br>
                 <h5>XYZ:  </h5><input id="xyz" type="text" name="xyz"><br><br>
             </div>
             <div>
