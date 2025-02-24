3f433af01e18b4d9d0e4cebf8a0777b0fcf0d010
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-24 17:39:30 +0000

    20250224

--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -2,32 +2,33 @@
 <div class="container-fluid">
 <div>Novo Fornecedor</div>
 <br><br><br><br>
-    <form method="post" action="../public/Supplier">
-    <input type="hidden" name="op" value="insert">	
-        <div class="split left">
-        <div class="centered">
-            <h2>Jane Flex</h2>
-            <p>Some text.</p>
-        </div>
-        </div>
+    <div>
+        <form method="post" action="../public/Supplier">
+            <input type="hidden" name="op" value="insert">	
+            <div class="split left">
+            <div class="centered">
+                <h2>Jane Flex</h2>
+                <p>Some text.</p>
+            </div>
+            </div>
 
-        <div class="split right">
-        <div class="centered">
-            <h2>John Doe</h2>
-            <p>Some text here too.</p>
-        </div>
-        </div>
+            <div class="split right">
+            <div class="centered">
+                <h2>John Doe</h2>
+                <p>Some text here too.</p>
+            </div>
+            </div>
 
-        		
+                    
 
-        <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: white;">Nome:</div>
-        <input id="login" type="text" name="login"><br><br>
+            <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: white;">Nome:</div>
+            <input id="login" type="text" name="login"><br><br>
 
-        <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: white;">Data:</div>
-        <input id="pass" type="text" name="pass"><br><br><br>
+            <div style="font-size: 20px;font-family: sans-serif;margin: 10px;color: white;">Data:</div>
+            <input id="pass" type="text" name="pass"><br><br><br>
 
-        <input id="button" type="submit" value="Criar"><br><br>
+            <input id="button" type="submit" value="Criar"><br><br>
 
-    </form>
-         
+        </form>
+    </div>     
 </div>
