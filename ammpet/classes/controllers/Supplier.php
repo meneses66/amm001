a7f5bf1b268e7ef63666b122a5ad2c95c9a5e793
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-24 22:34:24 +0000

    20250224

--- ammpet/classes/controllers/Supplier.php
+++ ammpet/classes/controllers/Supplier.php
@@ -11,34 +11,8 @@ class Supplier {
     public function index()
     {
         //echo "This is Supplier controller";
-        //$this->view('supplier/supplier');
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-
-            //Get information from $_POST
-            $operation = $_POST['op'];
-
-            //Define inputs for DB operations:
-            $inputs[]="";
-
-            //Create new Login instance:
-            $init = new \Model\Supplier;
-
-            switch($operation)
-            {
-                case 'goto_new_supplier':
-                    $this->view('supplier/supplier-new');
-                break;
-
-                case 'goto_list_supplier':
-                    $this->view('supplier/supplier-list');
-                break;
-
-                case 'goto_update_supplier':
-                    $this->view('supplier/supplier-update');
-                break;
-            }
-            
-        }
+        $this->view('supplier/supplier');
+       
     }
 
     public function new_supplier(){
--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -6,13 +6,11 @@
         <div class="flex-container">
             <div>
                 <label for="name" class="medium-label">Nome: </label><input id="name" type="text" size="50" name="name"><br><br>
-                <h5>XYZ:  </h5><input id="xyz" type="text" name="xyz"><br><br>
+                <label for="xyz" class="medium-label">XYZ: </label><input id="xyz" type="text" size="30" name="xyz"><br><br>
             </div>
             <div>
-                <h5>Tipo:</h5>
-                <input id="type" type="text" size="20" name="type"><br><br>
-                <h5>KLM:</h5>
-                <input id="klm" type="text" name="klm"><br><br>
+                <label for="type" class="medium-label">Tipo: </label><input id="type" type="text" size="20" name="type"><br><br>
+                <label for="klm" class="medium-label">KLM: </label><input id="klm" type="text" size="20" name="klm"><br><br>
             </div>
             <div>
             </div>
