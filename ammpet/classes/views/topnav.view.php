fa8ca749ade726b01709743e4f2bcc2177ee26e3
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-25 14:03:07 +0000

    20250225

--- ammpet/classes/views/topnav.view.php
+++ ammpet/classes/views/topnav.view.php
@@ -5,22 +5,8 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                 <li class="nav-item"><a class="nav-link"><?php if(!isset($_SESSION['username'])) {session_start();} echo "Usuário: ".$_SESSION['username'];?></a></li></li>
-                <li class="nav-item active"><a class="nav-link" href="?run=logout">Sair</a></li>
+                <li class="nav-item active"><a class="nav-link" href="<?php echo ROOT."/Login";?>">Sair</a></li>
             </ul>
         </div>
     </div>
 </nav>
-
-<?php
-
-if (isset($_GET['run'])) $linkchoice=$_GET['run'];
-else $linkchoice='';
-
-switch($linkchoice){
-
-case 'logout' :
-  redirect('Login');
-  break;
-
-}
-?>
