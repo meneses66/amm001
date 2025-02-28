eaffd2ad6c3fc23a8b33627c64dc895cc2965e60
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 11:06:47 +0000

    20250228

--- ammpet/classes/views/supplier/main-list.view.php
+++ ammpet/classes/views/supplier/main-list.view.php
@@ -1,7 +1,6 @@
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 <link href="https://cdn.datatables.net/v/bs4/dt-2.2.2/datatables.min.css" rel="stylesheet" integrity="sha384-fTqd416qRc9kwY299KdgUPsjOvS5bwkeE6jlibx2m7eL3xKheqGyU48QCFgZAyN4" crossorigin="anonymous">
-<link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css">
 
 <div class="container">
     <div class="row">
