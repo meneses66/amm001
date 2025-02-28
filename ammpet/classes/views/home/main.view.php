2a1e611e91b530d9c91ddcca003d2743ab09c689
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 19:42:54 +0000

    20250228

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,5 +1,6 @@
 <!-- <link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css"> -->
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <div class="container-fluid">
     <div>Home</div>
     <hr class="my-1">
@@ -13,9 +14,4 @@
         <div class="col-sm-6"><a href="<?php echo ROOT."/OrderX/new_orderx";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Nova Venda</a></div>
     </div>
     <br>
-
-
-
-
-
 </div>
