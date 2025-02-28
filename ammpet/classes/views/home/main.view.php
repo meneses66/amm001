dfd703960fac453161be34bae2cd448eed83c80c
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 19:02:27 +0000

    20250228

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -12,4 +12,18 @@
         <div class="col-sm-3"><a href="<?php echo ROOT."/OrderX/new_orderx";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Nova Venda</a></div>
         <div class="col-sm-3">.col-sm-3</div>
     </div>
+    <br>
+    <hr class="my-1">
+    <br>
+    <div class="card-footer d-flex justify-content-between">
+        <a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary m-1 btn-lg btn-block "><i class="fas fa-upload"></i>&nbsp;Clientes</a>
+        <a href="<?php echo ROOT."/OrderX/list_orderx";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Vendas</a>
+    </div>
+    <div class="card-footer d-flex justify-content-between">
+        <a href="<?php echo ROOT."/Agenda/week_view";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Agenda</a>
+        <a href="<?php echo ROOT."/OrderX/new_orderx";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Nova Venda</a>
+    </div>
+
+
+
 </div>
