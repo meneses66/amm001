c5a32ed9d4fbf2cc6ef791f8e6583e803d071eb3
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 18:13:16 +0000

    20250228

--- ammpet/classes/views/create/main.view.php
+++ ammpet/classes/views/create/main.view.php
@@ -4,26 +4,41 @@
 <div class="container-fluid">
 <div>Cadastros</div>
 <br><br><br><br>
+    <div class="row">
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Client/new_client";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Cliente</a></div>
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Supplier/new_supplier";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Fornecedor</a></div>
+        <div class="col-sm-3"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3"><a href="<?php echo ROOT."/ProdServ/new_product";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Produto</a></div>
+        <div class="col-sm-3"><a href="<?php echo ROOT."/ProdServ/new_service";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Serviço</a></div>
+        <div class="col-sm-3"></div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Breed/new_breed";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Nova Raça</a></div>
+        <div class="col-sm-3"></div>
+        <div class="col-sm-3"></div>
+    </div>
     <table style="width:100%">
         <tr>
             <td>
-                <a href="<?php echo ROOT."/Client/new_client";?>" class="btn btn-success m-1 float-center"><i class="fas fa-upload"></i>&nbsp;Novo Cliente</a>
+                
             </td>
             <td>
-                <a href="<?php echo ROOT."/Supplier/new_supplier";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Fornecedor</a>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <a href="<?php echo ROOT."/ProdServ/new_product";?>" class="btn btn-success m-1 float-center"><i class="fas fa-upload"></i>&nbsp;Novo Produto</a>
-            </td>
-            <td>
-                <a href="<?php echo ROOT."/ProdServ/new_service";?>" class="btn btn-success m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Serviço</a>
+                
             </td>
         </tr>
         <tr>
             <td>
-                <a href="<?php echo ROOT."/Breed/new_breed";?>" class="btn btn-success m-1 float-center"><i class="fas fa-upload"></i>&nbsp;Nova Raça</a>
+                
+            </td>
+            <td>
+                
+            </td>
+        </tr>
+        <tr>
+            <td>
+                
             </td>
         </tr>
     </table>           
