de13728632bc63c1dfd593167b4ac902b48f7003
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 19:18:16 +0000

    20250228

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,11 +1,11 @@
 <link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css">
-<div class="container-fluid">
+<div class="container">
     <div>Home</div>
     <br><br><br><br>
     <div class="row">
-        <div class="col-sm-3"><a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary m-1 btn-lg btn-block"><i class="fas fa-upload"></i>&nbsp;Clientes</a></div>
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Clientes</a></div>
         <div class="col-sm-3"><a href="<?php echo ROOT."/OrderX/list_orderx";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Vendas</a></div>
-        <div class="col-sm-3"><button type="button" class="btn btn-primary btn-lg btn-block">Block level button</button></div>
+        <div class="col-sm-3"><button type="button" class="btn btn-primary btn-lg m-1 btn-block">Block level button</button></div>
     </div>
     <div class="row">
         <div class="col-sm-3"><a href="<?php echo ROOT."/Agenda/week_view";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Agenda</a></div>
@@ -16,7 +16,7 @@
     <hr class="my-1">
     <br>
     <div class="card-footer d-flex justify-content-between">
-        <a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary m-1 btn-lg btn-block"><i class="fas fa-upload"></i>&nbsp;Clientes</a>
+        <a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Clientes</a>
         <a href="<?php echo ROOT."/OrderX/list_orderx";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Vendas</a>
     </div>
     <div class="card-footer d-flex justify-content-between">
