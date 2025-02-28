7dd5f1b342a533f7c20fcefb369fa8cac629ca63
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 22:25:45 +0000

    20250228

--- ammpet/classes/views/create/main.view.php
+++ ammpet/classes/views/create/main.view.php
@@ -10,15 +10,15 @@
     <hr class="my-1">
     <br><br><br><br>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Client/new_client";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Novo Cliente</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Supplier/new_supplier";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Novo Fornecedor</a></div>
+        <div class="col-sm-6"><a href="<?php echo ROOT."/Client/new_client";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-user-plus"></i>&nbsp;Novo Cliente</a></div>
+        <div class="col-sm-6"><a href="<?php echo ROOT."/Supplier/new_supplier";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-id-badge"></i>&nbsp;Novo Fornecedor</a></div>
     </div>
     <div class="row">
         <div class="col-sm-6"><a href="<?php echo ROOT."/ProdServ/new_product";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Novo Produto</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/ProdServ/new_service";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Novo Serviço</a></div>
+        <div class="col-sm-6"><a href="<?php echo ROOT."/ProdServ/new_service";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-paw"></i>&nbsp;Novo Serviço</a></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Breed/new_breed";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Nova Raça</a></div>
+        <div class="col-sm-6"><a href="<?php echo ROOT."/Breed/new_breed";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-dog"></i>&nbsp;Nova Raça</a></div>
         <div class="col-sm-6"></div>
     </div>
 </div>
