9a1f1e9041ba977908b353a068aea834619f5930
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 19:09:20 +0000

    20250228

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-sm-3"><a href="<?php echo ROOT."/Agenda/week_view";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Agenda</a></div>
         <div class="col-sm-3"><a href="<?php echo ROOT."/OrderX/new_orderx";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-upload"></i>&nbsp;Nova Venda</a></div>
-        <div class="col-sm-3">.col-sm-3</div>
+        <div class="col-sm-3"></div>
     </div>
     <br>
     <hr class="my-1">
