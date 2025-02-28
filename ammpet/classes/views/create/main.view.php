bfbdbbccc70ea7ee86b9a832b76d45f1324407de
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-02-28 18:32:33 +0000

    20250228

--- ammpet/classes/views/create/main.view.php
+++ ammpet/classes/views/create/main.view.php
@@ -2,8 +2,8 @@
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <div class="container-fluid">
-<div>Cadastros</div>
-<br><br><br><br>
+    <div>Cadastros</div>
+    <br><br><br><br>
     <div class="row">
         <div class="col-sm-3"><a href="<?php echo ROOT."/Client/new_client";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Cliente</a></div>
         <div class="col-sm-3"><a href="<?php echo ROOT."/Supplier/new_supplier";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Novo Fornecedor</a></div>
@@ -19,27 +19,4 @@
         <div class="col-sm-3"></div>
         <div class="col-sm-3"></div>
     </div>
-    <table style="width:100%">
-        <tr>
-            <td>
-                
-            </td>
-            <td>
-                
-            </td>
-        </tr>
-        <tr>
-            <td>
-                
-            </td>
-            <td>
-                
-            </td>
-        </tr>
-        <tr>
-            <td>
-                
-            </td>
-        </tr>
-    </table>           
 </div>
--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -1,35 +1,15 @@
 <link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css">
 <div class="container-fluid">
-<div>Home</div>
-<br><br><br><br>
-    <table style="width:100%">
-        <tr>
-            <td>
-                <form method="post" action="../public/Client">
-                    <input type="hidden" name="op" value="list_customer">
-                    <input id="button_main" type="submit" value="Clientes">
-                </form><br>
-            </td>
-            <td>
-                <form method="post" action="../public/OrderX">
-                    <input type="hidden" name="op" value="list_orders">
-                    <input id="button_main" type="submit" value="Vendas">
-                </form><br>
-            </td>
-        </tr>
-        <tr>
-            <td>
-                <form method="post" action="../public/Agenda">
-                    <input type="hidden" name="op" value="agenda">
-                    <input id="button_main" type="submit" value="Agenda">
-                </form>
-            </td>
-            <td>
-                <form method="post" action="../public/OrderX">
-                    <input type="hidden" name="op" value="new_order">
-                    <input id="button_main" type="submit" value="Nova Venda">
-                </form>
-            </td>
-        </tr>
-    </table>           
+    <div>Home</div>
+    <br><br><br><br>
+    <div class="row">
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Client/list_client";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Clientes</a></div>
+        <div class="col-sm-3"><a href="<?php echo ROOT."/OrderX/list_orderx";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Vendas</a></div>
+        <div class="col-sm-3">.col-sm-3</div>
+    </div>
+    <div class="row">
+        <div class="col-sm-3"><a href="<?php echo ROOT."/Agenda/week_view";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Agenda</a></div>
+        <div class="col-sm-3"><a href="<?php echo ROOT."/OrderX/new_orderx";?>" class="btn btn-primary btn-lg m-1 float-left"><i class="fas fa-upload"></i>&nbsp;Nova Venda</a></div>
+        <div class="col-sm-3">.col-sm-3</div>
+    </div>
 </div>
