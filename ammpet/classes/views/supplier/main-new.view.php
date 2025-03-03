297688e6579c3a063c426cd9f04db9302841e794
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-03 14:32:00 +0000

    20250303

--- ammpet/classes/views/supplier/main-new.view.php
+++ ammpet/classes/views/supplier/main-new.view.php
@@ -1,10 +1,18 @@
 <link rel="stylesheet" href="<?php echo ROOT."/";?>../public/assets/css/styles.css">
-    <div><h3>Novo Fornecedor</h3></div>
-    <br>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-lg-6">
+            <h4>Consultas</h4>
+        </div>
+    </div>
+    <hr class="my-1">
+    <br><br><br><br>
+
     <form method="post" action="../Supplier/insert">
-    <input type="hidden" name="op" value="insert">
-        <div class="flex-container">
-            <div>
+        <input type="hidden" name="op" value="insert">
+
+        <div class="row">
+            <div class="col-sm-6">
                 <input id="id" type="hidden" name="Id" value=""><br>
                 <input id="created_by" type="hidden" name="Created_by" value="<?php if(!isset($_SESSION['username'])) {session_start();} echo $_SESSION['username'];?>">
                 <input id="updated_by" type="hidden" name="Updated_by" value="<?php if(!isset($_SESSION['username'])) {session_start();} echo $_SESSION['username'];?>">
@@ -35,16 +43,19 @@
                     <option class="medium-label" value="Inativo">Inativo</option>
                 </select><br>
             </div>
-            <div>
+            <div class="col-sm-6">
                 <label for="CNPJ" class="medium-label">CNPJ: </label><input id="cnpj" type="text" size="20" name="CNPJ"><br>
                 <label for="CPF" class="medium-label">CPF: </label><input id="cpf" type="text" size="20" name="CPF"><br>
                 <label for="Sequence" class="medium-label">Sequencia: </label><input id="seq" type="text" size="10" name="Sequence"><br>
                 <label for="Hire_Date" class="medium-label">Data Inicio: </label><input id="start_date" type="date" size="30" name="Hire_Date"><br>
                 <label for="Comment" class="medium-label">Comentarios: </label><input id="comment" type="text" size="50" name="Comment"><br>
             </div>
-            <div>
-
+        </div>
+        <div class="row">
+            <div class="col-sm-6"></div>
+            <div class="col-sm-6">
+                <input id="button" type="submit" value="Criar">
             </div>
         </div>
-        <div align="right"><input id="button" type="submit" value="Criar"><br><br></div>
     </form>
+</div>
