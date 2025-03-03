38a1a5ed4c6037cdc77156593d035c6980fe649c
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-03 13:19:53 +0000

    20250303

--- ammpet/classes/views/sidebar.view.php
+++ ammpet/classes/views/sidebar.view.php
@@ -1,6 +1,7 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <div class="sidebar-heading border-bottom bg-light">ABC System</div>
   <div class="list-group list-group-flush">
-      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Home' && URL_1=="") echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home";?>">Home</a>
+      <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Home' && URL_1=="") echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home";?>"><i class="fas fa-search"></i>Home</a>
       <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='create') echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home/create";?>">Cadastros</a>
       <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='search') echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home/search";?>">Consultas</a>
       <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='report') echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home/report";?>">Relatórios</a>
