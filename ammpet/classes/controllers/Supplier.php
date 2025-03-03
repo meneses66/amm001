c19319a062bf42ce746e4a449b460b1f7f5c06fa
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-03 22:37:23 +0000

    20250303

--- ammpet/classes/controllers/Supplier.php
+++ ammpet/classes/controllers/Supplier.php
@@ -86,7 +86,7 @@ class Supplier {
         
     }
 
-    public function list_rows($inputs=null){
+    public function list_rows(){
         //if(isset($_POST['operation']) && $_POST['operation']==="view"){
             $output = "";
             $supplier = new \Model\Supplier;
