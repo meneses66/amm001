8cfc9bd07eb8979eb87fdf2ba7907b6c7b6afd01
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-06 12:26:28 +0000

    20250306

--- ammpet/classes/views/supplier/supplier-list.view.php
+++ ammpet/classes/views/supplier/supplier-list.view.php
@@ -27,7 +27,8 @@
         <script src="<?php echo ROOT;?>/assets/js/scripts.js"></script>
 
         <!-- jQuery library -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+        <script src="<?php echo ROOT;?>/assets/js/jquery-3.7.1.js"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script> -->
 
         <!-- Popper JS -->
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
