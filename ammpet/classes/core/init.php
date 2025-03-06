ab2a1c5de193e5eb1f09c256af6c2341df8102b9
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-06 13:44:27 +0000

    20250306

--- ammpet/classes/core/init.php
+++ ammpet/classes/core/init.php
@@ -4,7 +4,7 @@ defined('ROOTPATH') OR exit('Access denied!');
 
 require 'functions.php';
 require 'properties.php';
-require 'ajax_call.php';
+require 'controller/ajax_call.php';
 require '_DAO.php';
 require '_GlobalModel.php';
 require '_GlobalController.php';
