9a79c2b97ee826528a420aa105cdc1fdcc98437e
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-06 14:23:34 +0000

    20250306

--- ammpet/classes/views/supplier/supplier-list.view.php
+++ ammpet/classes/views/supplier/supplier-list.view.php
@@ -53,7 +53,7 @@
         
             function list_Rows(){
                         $.ajax({
-                            url: "<?=ROOTPATH_CLASSES?>core/ajax_call.php",
+                            url: "<?=ROOTPATH_CLASSES?>ajax_call.php",
                             type: "POST",
                             data: {operation:"view", class:"Supplier", function:"list_rows"},
                             success: function(response){
@@ -79,7 +79,7 @@
                     }).then((result) => {
                         if (result.isConfirmed) {
                             $.ajax({
-                                url:"<?=ROOTPATH_CLASSES?>core/ajax_call.php",
+                                url:"<?=ROOTPATH_CLASSES?>ajax_call.php",
                                 type: "POST",
                                 data:{del_id:del_id, class:"Supplier", method:"delete_call"},
                                 success:function(response){
