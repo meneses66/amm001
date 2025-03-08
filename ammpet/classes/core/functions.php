b4c946864e264682847520d88acf54eb2572197c
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-08 02:26:48 +0000

    20250307

--- ammpet/classes/core/functions.php
+++ ammpet/classes/core/functions.php
@@ -142,7 +142,7 @@ function load_options_update ($type, $status){
     $options = $params->getParamListByType($type, $status);
     if($options){
         foreach ($options as $option) { 
-            $selected='.(($data_form[\'TYPE\']=='.$option->VALUE.')?"selected":"").';
+            $selected='\'.(($data_form[\'TYPE\']=='.$option->VALUE.')?"selected":"").\'';
             $option_list .= '<option class="medium-label" value="'.$option->VALUE.'" '.$selected.'>'.$option->VALUE.'</option>';
         }
     }
