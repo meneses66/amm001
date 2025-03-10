a8fc7edc59e812a0dcdb0d6ec6d58f63282ea77b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-10 22:05:58 +0000

    20250310

--- ammpet/classes/controllers/Params.php
+++ ammpet/classes/controllers/Params.php
@@ -521,49 +521,45 @@ class Params {
 
     //LOAD HTML FOR LISTING RECORDS IN TABLE
     public function load_rows(){
-        //if(isset($_POST['operation']) && $_POST['operation']==="view"){
-            $output = "";
-            //$$this->object = new \Model.'\\'.$this->UCF_object;
-            $$this->object = new ('\Model\\'.$this->UCF_object);
-
-            $data = $$this->object->listAll();
-            if($this->object->countAll()>0){
-                $output .='<thead>
-                                <tr class="text-center text-secondary">
-                                    <th>Id</th>
-                                    <th>Atualiz.</th>
-                                    <th>Nome</th>
-                                    <th>Valor</th>
-                                    <th>Tipo</th>
-                                    <th>Status</th>
-                                    <th>Comentários</th>
-                                    <th>Ações</th>
-                                </tr>
-                            </thead>
-                            <tbody>';
-                foreach ($data as $row) {
-                    $output .='<tr class="text-center text-secondary">
-                                <td>'.$row->ID.'</td>
-                                <td>'.$row->UPDATED.'</td>
-                                <td>'.$row->NAME.'</td>
-                                <td>'.$row->VALUE.'</td>
-                                <td>'.$row->TYPE.'</td>
-                                <td>'.$row->STATUS.'</td>
-                                <td>'.$row->COMMENT.'</td>
-                                <td>
-                                    <a href="'.ROOT."/$this->UCF_object/_update?id=$row->ID".'" title="Edit" class="text-primary updateBtn" id="'.$row->ID.'"><i class="fas fa-edit"></i></a>&nbsp;&nbsp;
-                                    <a href="'.ROOT."/$this->UCF_object/_delete?id=$row->ID".'" title="Delete" class="text-danger deleteBtn" id="'.$row->ID.'"><i class="fas fa-eraser"></i></a>
-                                </td></tr>';
-                }
-                $output .= '</tbody>';
-                echo $output;
+            
+        $output = "";
+        $$this->object = new \Model\Params;
+        
+        $data = $$this->object->listAll();
+        if($this->object->countAll()>0){
+            $output .='<thead>
+                            <tr class="text-center text-secondary">
+                                <th>Id</th>
+                                <th>Atualiz.</th>
+                                <th>Nome</th>
+                                <th>Valor</th>
+                                <th>Tipo</th>
+                                <th>Status</th>
+                                <th>Comentários</th>
+                                <th>Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>';
+            foreach ($data as $row) {
+                $output .='<tr class="text-center text-secondary">
+                            <td>'.$row->ID.'</td>
+                            <td>'.$row->UPDATED.'</td>
+                            <td>'.$row->NAME.'</td>
+                            <td>'.$row->VALUE.'</td>
+                            <td>'.$row->TYPE.'</td>
+                            <td>'.$row->STATUS.'</td>
+                            <td>'.$row->COMMENT.'</td>
+                            <td>
+                                <a href="'.ROOT."/$this->UCF_object/_update?id=$row->ID".'" title="Edit" class="text-primary updateBtn" id="'.$row->ID.'"><i class="fas fa-edit"></i></a>&nbsp;&nbsp;
+                                <a href="'.ROOT."/$this->UCF_object/_delete?id=$row->ID".'" title="Delete" class="text-danger deleteBtn" id="'.$row->ID.'"><i class="fas fa-eraser"></i></a>
+                            </td></tr>';
             }
-            else{
-                echo '<h3 class="text-center text-secondary mt-5">Sem dados para mostrar</h3>';
-            }
-        //}
+            $output .= '</tbody>';
+            echo $output;
+        }
+        else{
+            echo '<h3 class="text-center text-secondary mt-5">Sem dados para mostrar</h3>';
+        }
     }
 
-
 }
-
