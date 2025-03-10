bd56f686986c5ebb747b6f9be50ceff5372c71e4
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-10 22:55:45 +0000

    20250310

--- ammpet/classes/core/_GlobalController.php
+++ ammpet/classes/core/_GlobalController.php
@@ -32,4 +32,79 @@ Trait _GlobalController{
             require $fileName;
         }
     }
+
+
+    // FUNCTIONS MOVED FROM PARAMS TO BE STANDARDIZED
+
+    //SESSION TO DEFINE TO WHICH PAGE USER WILL BE SENT: NEW, UPDATE, DELETE, LIST
+
+    //Sends to page to create NEW Params
+    public function _new(){
+
+        $operation = 'goto_new';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $operation = $_POST['operation'];
+        }
+        $this->goto_view($operation);
+    }
+
+    //Sends to LIST View
+    public function _list(){
+        $operation = 'goto_list';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $operation = $_POST['operation'];
+        }
+        $this->goto_view($operation);
+    }
+
+    //Sends to UPDATE View
+    public function _update(){
+
+        $operation = 'goto_update';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $operation = $_POST['operation'];
+        }
+        $this->goto_view($operation);
+    }
+
+    //Sends to DELETE View
+    public function _delete(){
+
+        $operation = 'goto_delete';
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $operation = $_POST['operation'];
+        }
+        $this->goto_view($operation);
+    }
+
+    //Defines view to go to
+    private function goto_view($operation){
+    
+        switch($operation)
+        {
+            case 'goto_new':
+                $view="$this->object/$this->object-new";
+                $this->view($view);
+            break;
+
+            case 'goto_list':
+                $view="$this->object/$this->object-list";
+                $this->view($view);
+            break;
+
+            case 'goto_update':
+                $view="$this->object/$this->object-update";
+                $this->view($view);
+            break;
+
+            case 'goto_delete':
+                $view="$this->object/$this->object-delete";
+                $this->view($view);
+            break;
+        }
+    }
+
 }
+
+
+    
