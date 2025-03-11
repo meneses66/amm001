7dfe7b425c26ea9b8937d5e83d12df89bf5303c5
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-11 20:03:19 +0000

    20250311

--- ammpet/public/assets/js/tablejs.php
+++ ammpet/public/assets/js/tablejs.php
@@ -22,7 +22,7 @@ $output = '<script type="text/javascript">
                                                         targets: 1,
                                                         render: DataTable.render.datetime(\'DD-MM-YYYY\')
                                                     }
-                                                ]
+                                                ],
                                     "order": [[ 1, "desc" ]]
                                 });
                         }
