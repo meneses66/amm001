30195e832e1e6669eef8674f8dc24f093cbd23e6
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-12 14:43:31 +0000

    20250312

--- ammpet/public/assets/js/showpermission.js
+++ ammpet/public/assets/js/showpermission.js
@@ -8,14 +8,24 @@ function showHidePermission(){
 
 
 let checkboxes = document.querySelectorAll("input[type=checkbox]");
+let enabledSettings = [];
 
 checkboxes.forEach(function(checkbox) {
     checkbox.addEventListener('change', function() {
-    if (this.checked) {
-        console.log("Checkbox is checked..");
-    } else {
-        console.log("Checkbox is not checked..");
-    }
+
+        if (this.checked) {
+            console.log("Checkbox is checked..");
+        } else {
+            console.log("Checkbox is not checked..");
+        }
+
+        enabledSettings = 
+        Array.from(checkboxes) // Convert checkboxes to an array to use filter and map.
+        .filter(i => i.checked) // Use Array.filter to remove unchecked checkboxes.
+        .map(i => i.value); // Use Array.map to extract only the checkbox values from the array of objects.
+      
+        console.log(enabledSettings);
+        
     })
 });
 
