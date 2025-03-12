3313bae875aa8b1fc24ab19ac91f82d736ef1798
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-12 12:09:43 +0000

    20250312

--- ammpet/public/assets/js/showpermission.js
+++ ammpet/public/assets/js/showpermission.js
@@ -1,7 +1,7 @@
 function showHidePermission(){
-    if(document.getElementById('login').value !=null){
-        document.getElementById('permission').style.display='none';
-    } else {
+    if(document.getElementById('login').value ==null){
         document.getElementById('permission').style.display='block';
+    } else {
+        document.getElementById('permission').style.display='none';
     }
 }
