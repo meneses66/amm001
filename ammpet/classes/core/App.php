6a55ac8e6ea9c3ebc341b1ade1adde7a8a27cee4
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-13 23:02:41 +0000

    20250313

--- ammpet/classes/core/App.php
+++ ammpet/classes/core/App.php
@@ -2,7 +2,8 @@
 
 namespace Core;
 
-(defined('ROOTPATH') AND isset($_SESSION['username']) AND ($_SESSION['username']!="" || $_SESSION['username']!=null  )) OR exit('Access denied!');
+//(defined('ROOTPATH') AND isset($_SESSION['username']) AND ($_SESSION['username']!="" || $_SESSION['username']!=null  )) OR exit('Access denied!');
+defined('ROOTPATH') OR exit('Access denied!');
 
 class App{
     
