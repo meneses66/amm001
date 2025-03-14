e0b97726f3c58749195e931c5e62b8486f0b41f5
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-14 01:38:24 +0000

    20250313

--- ammpet/classes/controllers/ProdServ.php
+++ ammpet/classes/controllers/ProdServ.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Controller;
+session_start();
 
 //defined('ROOTPATH') OR exit('Access denied!');
 (defined('ROOTPATH') AND isset($_SESSION['username']) AND ($_SESSION['username']!="" || $_SESSION['username']!=null  )) OR exit('Access denied!');
