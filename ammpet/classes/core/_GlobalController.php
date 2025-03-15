bcdb127455bc83648326341c596383ff4a5af811
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-15 21:30:39 +0000

    20250315

--- ammpet/classes/core/_GlobalController.php
+++ ammpet/classes/core/_GlobalController.php
@@ -254,6 +254,26 @@ Trait _GlobalController{
             unset($inputs["Created"]);
             unset($inputs["Updated"]);
 
+            //ADJUST FLAGS TO 0 or 1 in PRODUCT:
+
+            if($this->UCF_object=="Product"){
+                if(isset($inputs["Comission_flg"])){
+                    $inputs["Comission_flg"]="1";
+                }else{
+                    $inputs["Comission_flg"]="0";
+                }
+                if(isset($inputs["Comission_overwrite_flg"])){
+                    $inputs["Comission_overwrite_flg"]="1";
+                }else{
+                    $inputs["Comission_overwrite_flg"]="0";
+                }
+                if(isset($inputs["Flag1"])){
+                    $inputs["Flag1"]="1";
+                }else{
+                    $inputs["Flag1"]="0";
+                }
+            }
+            
             //START UNSET CHECKBOXES IN SUPPLIET VIEW:
 
             if($this->UCF_object=="Supplier"){
