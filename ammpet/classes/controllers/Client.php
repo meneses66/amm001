bc307ecb6b1405a0a01efc68731d8e0f3e2719a5
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-17 21:53:36 +0000

    20250317

--- ammpet/classes/controllers/Client.php
+++ ammpet/classes/controllers/Client.php
@@ -52,13 +52,13 @@ class Client
                             <label for="mobile_1" class="medium-label">Celular 1:</label>
                         </div>
                         <div class="col-sm-3">
-                            <input id="mobile_1" type="text" size="25" name="Mobile_1" placeholder="+55 XX XXXXX XXXX" pattern="+[0-9]{2} [0-9]{2} [0-9]{5} [0-9]{4}">
+                            <input id="mobile_1" type="text" size="25" name="Mobile_1" placeholder="+55-##-#####-####" pattern="+[0-9]{2}-[0-9]{2}-[0-9]{5}-[0-9]{4}">
                         </div>
                         <div class="col-sm-1">
                             <label for="mobile_2" class="medium-label">Celular 2:</label>
                         </div>
                         <div class="col-sm-3">
-                            <input id="mobile_2" type="text" size="25" name="Mobile_2" placeholder="+55 XX XXXXX XXXX" pattern="+[0-9]{2} [0-9]{2} [0-9]{5} [0-9]{4}">
+                            <input id="mobile_2" type="text" size="25" name="Mobile_2" placeholder="+55-##-#####-####" pattern="+[0-9]{2}-[0-9]{2}-[0-9]{5}-[0-9]{4}">
                         </div>
                     </div><br>
                     <div class="row">
