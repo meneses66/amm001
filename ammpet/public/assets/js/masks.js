68eb95100ae62c3cb96f39f41f535157595f8647
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-18 14:06:43 +0000

    20250318

---
+++ ammpet/public/assets/js/masks.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    /** 
+    $('.date').mask('00/00/0000');
+    $('.time').mask('00:00:00');
+    $('.date_time').mask('00/00/0000 00:00:00');
+    $('.cep').mask('00000-000');
+    $('.phone').mask('0000-0000');
+    $('.phone_with_ddd').mask('(00) 0000-0000');
+    $('.phone_us').mask('(000) 000-0000');
+    $('.cpf').mask('000.000.000-00', {reverse: true});
+    $('.cnpj').mask('00.000.000/0000-00', {reverse: true});
+    $('.money').mask('000.000.000.000.000,00', {reverse: true});
+    $('.money2').mask("#.##0,00", {reverse: true});
+    $('.percent').mask('##0,00%', {reverse: true});
+    $('.placeholder').mask("00/00/0000", {placeholder: "__/__/____"});
+    */
+
+    $('#mobile_1').mask('(00) 00000-0000');
+
+  });
+  
