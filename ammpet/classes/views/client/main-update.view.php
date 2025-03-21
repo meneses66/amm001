1ab89aa1f3b9baf127d4c13581c2646a7c43ffb5
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-21 17:17:36 +0000

    20250321

--- ammpet/classes/views/client/main-update.view.php
+++ ammpet/classes/views/client/main-update.view.php
@@ -6,18 +6,7 @@
         <div class="col-sm-4">
             <h4>Atualizar Cliente</h4>
         </div>
-        <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_animal?cli_id=".$_GET['id'];?>" title="Animals" class="<?php echo $class=(URL_0=='Client' && URL_1=='_cli_animal') ? 'btn btn-secondary btn-lg m-1 btn-block' : 'btn btn-outline-secondary btn-lg m-1 btn-block'; ?>" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Animais</a>
-        </div>
-        <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_package?cli_id=".$_GET['id'];?>" title="Packages" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Pacotes</a>
-        </div>
-        <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_product?cli_id=".$_GET['id'];?>" title="Products" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Produtos</a>
-        </div>
-        <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_service?cli_id=".$_GET['id'];?>" title="Services" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Serviços</a>
-        </div>
+        <?php include "client-nav.view.php";?>
     </div>
     <hr class="my-1">
     <br>
