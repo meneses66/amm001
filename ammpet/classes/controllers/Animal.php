0c6228ae0fe1556b6a3b6fdadf6ebbe5c00075f0
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-21 22:19:17 +0000

    20250321

--- ammpet/classes/controllers/Animal.php
+++ ammpet/classes/controllers/Animal.php
@@ -181,7 +181,7 @@ class Animal {
 
                 //START TO LOAD THE UPDATE FORM:
                 $output .= '<div class="row">
-                                <div class="col-sm-1>
+                                <div class="col-sm-1">
                                     <label for="id" class="medium-label">Id:</label>
                                 </div>
                                 <div class="col-sm-3">
--- ammpet/public/assets/js/load_breed_update_form.js
+++ ammpet/public/assets/js/load_breed_update_form.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     breedType = document.getElementById("type").value;
     temp_breed = document.getElementById("temp_breed").value;
+    console.log(breedType + " - " + temp_breed);
     load_breed(breedType, temp_breed, "update");
 });
