b52ef4065738c59388a690d308dc534168ed898d
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-21 15:26:12 +0000

    20250321

--- ammpet/classes/views/client/main-update.view.php
+++ ammpet/classes/views/client/main-update.view.php
@@ -7,16 +7,16 @@
             <h4>Atualizar Cliente</h4>
         </div>
         <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_animal?cli_id=".$_GET['cli_id'];?>" title="Animals" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['cli_id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Animais</a>
+            <a href="<?php echo ROOT."/Client/_cli_animal?cli_id=".$_GET['id'];?>" title="Animals" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Animais</a>
         </div>
         <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_package?cli_id=".$_GET['cli_id'];?>" title="Packages" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['cli_id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Pacotes</a>
+            <a href="<?php echo ROOT."/Client/_cli_package?cli_id=".$_GET['id'];?>" title="Packages" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Pacotes</a>
         </div>
         <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_product?cli_id=".$_GET['cli_id'];?>" title="Products" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['cli_id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Produtos</a>
+            <a href="<?php echo ROOT."/Client/_cli_product?cli_id=".$_GET['id'];?>" title="Products" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Produtos</a>
         </div>
         <div class="col-sm-2">
-            <a href="<?php echo ROOT."/Client/_cli_service?cli_id=".$_GET['cli_id'];?>" title="Services" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['cli_id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Serviços</a>
+            <a href="<?php echo ROOT."/Client/_cli_service?cli_id=".$_GET['id'];?>" title="Services" class="btn btn-info btn-lg m-1 btn-block" cli_id="<?php echo $_GET['id'];?>"><i class="fas fa-edit"></i>&nbsp;&nbsp;Serviços</a>
         </div>
     </div>
     <hr class="my-1">
