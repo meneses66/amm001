1f990cfcc3ad741a211741d9c97acb9f2c317815
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-22 03:02:08 +0000

    20250321

--- ammpet/public/assets/js/load_breed_update_form.js
+++ ammpet/public/assets/js/load_breed_update_form.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     breedType = document.getElementById("type").value;
     temp_breed = document.getElementById("temp_breed").value;
-    load_breed(breedType, temp_breed, 'update');
+    load_breed(breedType, temp_breed, 'kkkk');
 });
