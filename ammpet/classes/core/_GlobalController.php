8ec48a980fa9cb4ae39851bd27e32ff4a11ef4f2
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-24 17:34:49 +0000

    20250324

--- ammpet/classes/core/_GlobalController.php
+++ ammpet/classes/core/_GlobalController.php
@@ -232,21 +232,20 @@ Trait _GlobalController{
                 $inputs[$key]=$value;    
             }
 
-            //Special condition when Object is Orderx:
-            if($this->UFC_object="Orderx" AND isset($_GET['cli_id'])){
-                //$inputs['Id_client']=$_GET['cli_id'];
-                $inputs['Created_by']=$_SESSION['username'];
-                $inputs['Updated_by']=$_SESSION['username'];
-                unset($inputs["class"]);
-                unset($inputs["method"]);
-            }
-
             //Remove items from array inputs that are not columns in DB (op) or are auto-increment (Id)
             unset($inputs["operation"]);
             unset($inputs["Id"]);
             //Remove items from array inputs that are populated automatically in DB
             unset($inputs["Created"]);
             unset($inputs["Updated"]);
+
+            //Special condition when Object is Orderx:
+            if($this->UFC_object="Orderx" AND isset($_GET['cli_id'])){
+                $inputs['Created_by']=$_SESSION['username'];
+                $inputs['Updated_by']=$_SESSION['username'];
+                unset($inputs["class"]);
+                unset($inputs["method"]);
+            }
 
             //ADJUST FLAGS TO 0 or 1 in PRODUCT and SERVICE:
 
