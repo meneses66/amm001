6d4b3b182ed053ea7c888ad1edddd813e1971ca0
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-25 21:33:55 +0000

    20250325

--- ammpet/classes/views/orderx/main-order_details.view.php
+++ ammpet/classes/views/orderx/main-order_details.view.php
@@ -13,7 +13,7 @@
             <a href="<?php echo ROOT."/Orderx/_list";?>" class="btn btn-secondary btn-lg m-1 btn-block">Voltar</a>
         </div>
     </div>
-    <div class="row" style="border: thin solid gray">
+    <div class="row" style="border: thin solid light-gray">
             <?php 
                 $controller = new ('\Controller\\'."Orderx");
                 $controller->get_header();
