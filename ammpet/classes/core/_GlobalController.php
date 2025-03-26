34d916af430cf902c8557011b29f8c1932368d9c
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-26 21:25:14 +0000

    20250326

--- ammpet/classes/core/_GlobalController.php
+++ ammpet/classes/core/_GlobalController.php
@@ -181,7 +181,7 @@ Trait _GlobalController{
             $service = $_GET['service'];
             echo var_dump($service);
             //$service_array = explode('¡', $service);
-            $service_array = json_decode($service , true);
+            $service_array = json_decode($service);
             echo var_dump($service_array);
             $_SERVER['REQUEST_METHOD'] = 'POST';
             $_POST['class']="OrderItem";
