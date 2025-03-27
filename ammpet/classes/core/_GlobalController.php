a8816ac086c960f64dfd40fc30cf615fc5a7dffb
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-27 17:38:44 +0000

    20250327

--- ammpet/classes/core/_GlobalController.php
+++ ammpet/classes/core/_GlobalController.php
@@ -198,6 +198,8 @@ Trait _GlobalController{
             $service_array = json_decode2(html_entity_decode($service), true);
             //$service_array  = unserialize($service);
             echo var_dump($service_array);
+            echo var_dump("<br>");
+            echo var_dump($inputs);
             $_SERVER['REQUEST_METHOD'] = 'POST';
             $_POST['class']="OrderItem";
             $_POST['method']="insert_call";
