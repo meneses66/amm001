77c946531e896fc5690ac84bd68e15b9cf83b1f9
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-29 03:18:44 +0000

    20250329

--- ammpet/classes/views/orderx/main-order_details.view.php
+++ ammpet/classes/views/orderx/main-order_details.view.php
@@ -26,7 +26,7 @@
     </div>
     <div class="row">
         <div class="col-sm-6" style="border: thin solid lightgray; font-size:12px;">
-            <p style="font-size:12px;" >Animais</p>
+            <p style="font-size:12px font-weight:bold;" >Animais</p>
             <div class="table-responsive" id="_animals" style="font-size:12px;">
                 <table id="_table_ani" class="table Table-stripped table-sm table-bordered small">
                     <?php 
@@ -37,7 +37,7 @@
             </div>
         </div>
         <div class="col-sm-6" style="border: thin solid lightgray; font-size:12px;">
-            <p style="font-size:10px;" >Pacotes</p>
+            <p style="font-size:12px;" >Pacotes</p>
             <div class="table-responsive" id="_packages" style="font-size:12px;">
                 <table id="_table_pkg" class="table Table-stripped table-sm table-bordered small">
                     <?php 
