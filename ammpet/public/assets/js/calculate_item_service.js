2b66eaac10933a4005e4a62af826fc6441011e57
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-03-29 22:15:16 +0000

    20250329

--- ammpet/public/assets/js/calculate_item_service.js
+++ ammpet/public/assets/js/calculate_item_service.js
@@ -4,12 +4,12 @@ function calculate_item_service(input){
     let quantity = document.getElementById("quantity").value;
     let unit_value = document.getElementById("unit_value").value;
     let discount_value = document.getElementById("discount_value").value;
-    let price_cash = document.getElementById("price_cash").value;
-    let price_pix = document.getElementById("price_pix").value;
+    let oi_price_cash = document.getElementById("oi_price_cash").value;
+    let oi_price_pix = document.getElementById("oi_price_pix").value;
 
     if (id_package=="0") {
-        var total_cash = quantity*price_cash;
-        var total_pix = quantity*price_pix;
+        var total_cash = quantity*oi_price_cash;
+        var total_pix = quantity*oi_price_pix;
         var total_no_discount = quantity*unit_value;
         var total_with_discount = quantity*unit_value - discount_value;        
     } else{
