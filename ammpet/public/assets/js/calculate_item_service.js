96314b798c965122e5f0aacad1a2798fc5f2bbec
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-01 20:01:37 +0000

    20250401

--- ammpet/public/assets/js/calculate_item_service.js
+++ ammpet/public/assets/js/calculate_item_service.js
@@ -1,5 +1,4 @@
 function calculate_item_service(input){
-    console.log("Entrei - 0");
     let id_package = document.getElementById("id_package").value;
     let quantity = document.getElementById("quantity").value;
     let unit_value = document.getElementById("unit_value").value;
@@ -8,17 +7,16 @@ function calculate_item_service(input){
     let oi_price_pix = document.getElementById("oi_price_pix").value;
 
     if (id_package=="1" || id_package==1) {
-        console.log("Entrei - 3");
-        var total_cash = round(quantity*oi_price_cash, 2);
-        var total_pix = round(quantity*oi_price_pix, 2);
-        var total_no_discount = round(quantity*unit_value, 2);
-        var total_with_discount = round((round(quantity*unit_value, 2) - discount_value),2);
+        var total_cash = round(quantity*oi_price_cash, 2).toFixed(2);
+        var total_pix = round(quantity*oi_price_pix, 2).toFixed(2);
+        var total_no_discount = round(quantity*unit_value, 2).toFixed(2);
+        var total_with_discount = round((round(quantity*unit_value, 2) - discount_value),2).toFixed(2);
         document.getElementById("total_pix").value = total_pix;
         document.getElementById("total_cash").value = total_cash;
         document.getElementById("value_no_discount").value = total_no_discount;
         document.getElementById("value_with_discount").value = total_with_discount;       
     } else{
-        console.log("Entrei - 4");
+        //console.log("Entrei - 4");
         var total_cash = "0.00";
         var total_pix = "0.00";
         var total_no_discount = "0.00";
@@ -29,12 +27,6 @@ function calculate_item_service(input){
         document.getElementById("value_with_discount").value = total_with_discount;
         update_sequence(id_package);
     }
-
-    //document.getElementById("total_pix").value = total_pix;
-    //document.getElementById("total_cash").value = total_cash;
-    //document.getElementById("value_no_discount").value = total_no_discount;
-    //document.getElementById("value_with_discount").value = total_with_discount;
-
 }
 
 function round(num, decimalPlaces = 0) {
