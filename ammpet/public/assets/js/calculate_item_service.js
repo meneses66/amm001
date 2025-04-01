137b16040d31944ed31cfdd74250b4be09e754c2
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-01 19:48:29 +0000

    20250401

--- ammpet/public/assets/js/calculate_item_service.js
+++ ammpet/public/assets/js/calculate_item_service.js
@@ -12,20 +12,28 @@ function calculate_item_service(input){
         var total_cash = round(quantity*oi_price_cash, 2);
         var total_pix = round(quantity*oi_price_pix, 2);
         var total_no_discount = round(quantity*unit_value, 2);
-        var total_with_discount = round((round(quantity*unit_value, 2) - discount_value),2);        
+        var total_with_discount = round((round(quantity*unit_value, 2) - discount_value),2);
+        document.getElementById("total_pix").value = total_pix;
+        document.getElementById("total_cash").value = total_cash;
+        document.getElementById("value_no_discount").value = total_no_discount;
+        document.getElementById("value_with_discount").value = total_with_discount;       
     } else{
         console.log("Entrei - 4");
         var total_cash = "0.00";
         var total_pix = "0.00";
         var total_no_discount = "0.00";
         var total_with_discount = "0.00";
+        document.getElementById("total_pix").value = total_pix;
+        document.getElementById("total_cash").value = total_cash;
+        document.getElementById("value_no_discount").value = total_no_discount;
+        document.getElementById("value_with_discount").value = total_with_discount;
         update_sequence(id_package);
     }
 
-    document.getElementById("total_pix").value = total_pix;
-    document.getElementById("total_cash").value = total_cash;
-    document.getElementById("value_no_discount").value = total_no_discount;
-    document.getElementById("value_with_discount").value = total_with_discount;
+    //document.getElementById("total_pix").value = total_pix;
+    //document.getElementById("total_cash").value = total_cash;
+    //document.getElementById("value_no_discount").value = total_no_discount;
+    //document.getElementById("value_with_discount").value = total_with_discount;
 
 }
 
