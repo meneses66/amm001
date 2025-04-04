56b5a4eb4976be10197f489e8c85674b417c8ea0
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-04 15:04:49 +0000

    20250404

--- ammpet/classes/views/orderx/main-order_details.view.php
+++ ammpet/classes/views/orderx/main-order_details.view.php
@@ -48,42 +48,45 @@
             </div>
         </div>
     </div>
-    <div class="row" style="border: thin solid lightgray; font-size:11px;">
-        <div class="col-sm-2">
-            <p style="font-size:12px; font-weight:bold;">Serviços</p>
+    <Session style="border: thin solid lightgray; font-size:11px;">
+        <div class="row" style="border: thin solid lightgray; font-size:11px;">
+            <div class="col-sm-2">
+                <p style="font-size:12px; font-weight:bold;">Serviços</p>
+            </div>
+            <div class="col-sm-4" style="border-right: thin solid lightgray;>
+                <a href="<?php echo ROOT."/Orderx/_new_service?cli_id=".$_GET['cli_id']."&order_id=".$_GET['order_id'];?>" class="btn btn-primary btn-sm m-1 btn-block">Novo Serviço</a>
+            </div>
+            <div class="col-sm-2">
+                <p style="font-size:12px; font-weight:bold;">Produtos</p>
+            </div>
+            <div class="col-sm-4">
+                <a href="<?php echo ROOT."/Orderx/_new_product?cli_id=".$_GET['cli_id']."&order_id=".$_GET['order_id'];?>" class="btn btn-primary btn-sm m-1 btn-block">Novo Produto</a>
+            </div>
         </div>
-        <div class="col-sm-4">
-            <a href="<?php echo ROOT."/Orderx/_new_service?cli_id=".$_GET['cli_id']."&order_id=".$_GET['order_id'];?>" class="btn btn-primary btn-sm m-1 btn-block">Novo Serviço</a>
-        </div>
-        <div class="col-sm-2">
-            <p style="font-size:12px; font-weight:bold;">Produtos</p>
-        </div>
-        <div class="col-sm-4">
-            <a href="<?php echo ROOT."/Orderx/_new_product?cli_id=".$_GET['cli_id']."&order_id=".$_GET['order_id'];?>" class="btn btn-primary btn-sm m-1 btn-block">Novo Produto</a>
-        </div>
-    </div>
-    <div class="row" style="border: thin solid lightgray; font-size:11px;">
-        <div class="col-sm-6" style="border: thin solid lightgray; font-size:11px;">
-            <div class="table-responsive" id="_services" style="font-size:11px;">
-                    <table id="_table_services" class="table Table-stripped table-sm table-bordered small">
+        <div class="row" style="border: thin solid lightgray; font-size:11px;">
+            <div class="col-sm-6" style="border: thin solid lightgray; font-size:11px;">
+                <div class="table-responsive" id="_services" style="font-size:11px;">
+                        <table id="_table_services" class="table Table-stripped table-sm table-bordered small">
+                            <?php 
+                                $controller = new ('\Controller\\'."Orderx");
+                                $controller->get_services();
+                            ?>        
+                        </table>
+                    </div>
+                </div>
+            <div class="col-sm-6" style="border: thin solid lightgray; font-size:11px;">
+                <div class="table-responsive" id="_products" style="font-size:11px;">
+                    <table id="_table_products" class="table Table-stripped table-sm table-bordered small">
                         <?php 
                             $controller = new ('\Controller\\'."Orderx");
-                            $controller->get_services();
+                            $controller->get_products();
                         ?>        
                     </table>
                 </div>
             </div>
-        <div class="col-sm-6" style="border: thin solid lightgray; font-size:11px;">
-            <div class="table-responsive" id="_products" style="font-size:11px;">
-                <table id="_table_products" class="table Table-stripped table-sm table-bordered small">
-                    <?php 
-                        $controller = new ('\Controller\\'."Orderx");
-                        $controller->get_products();
-                    ?>        
-                </table>
-            </div>
         </div>
-    </div>
+    </Session>
+    
     <div class="row" style="border: thin solid lightgray; font-size:11px;">
         <div class="col-sm-6">
             <p style="font-size:12px; font-weight:bold;">Pagamentos</p>
