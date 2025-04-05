12994248d6caa44ac79a866098d4cdc36911b5f4
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-05 19:02:30 +0000

    20250405

--- ammpet/classes/views/params/params-list.view.php
+++ ammpet/classes/views/params/params-list.view.php
@@ -53,5 +53,15 @@ $_SESSION['LAST_ACTIVE']=time();?>
         <!-- Table Column Date Formatting-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
 
+        <!-- Export to Excel -->
+        <script src="https://cdn.datatables.net/buttons/3.2.2/js/dataTables.buttons.js"></script>
+        <script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.dataTables.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+        <script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.html5.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/3.2.2/js/buttons.print.min.js"></script>
+
+
     </body>
 </html>
