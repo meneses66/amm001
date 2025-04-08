e0a33f5a1596d29209318829147ca8515699fe8d
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-08 15:52:15 +0000

    20250408

--- ammpet/public/assets/js/selectors_salary.js
+++ ammpet/public/assets/js/selectors_salary.js
@@ -1,11 +1,11 @@
 function load_employee(dataformeemployee){  
-    //console.log("dataformeemployee-"+dataformeemployee);
+    console.log("dataformeemployee-"+dataformeemployee);
     $.ajax({
       url: "/ammpet/public/Ajax_call",
       type: "POST",
       data: {class:"Supplier", method:"load_employee_options",  dfemployee: dataformeemployee},
       success: function(response){
-        //console.log(response);
+        console.log(response);
           $('#id_employee').html(response);
       }
   });
