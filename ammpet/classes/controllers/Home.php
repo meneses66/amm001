a27f985e84ec200e5e35a1d9541c0e22e6fa4525
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-11 14:53:03 +0000

    20250411

--- ammpet/classes/controllers/Home.php
+++ ammpet/classes/controllers/Home.php
@@ -2,12 +2,7 @@
 
 namespace Controller;
 
-//if(session_status() === PHP_SESSION_NONE) session_start();
-
-if(session_status() === PHP_SESSION_NONE){
-    my_session_start();
-    my_session_regenerate_id();
-}
+if(session_status() === PHP_SESSION_NONE) session_start();
 
 (defined('ROOTPATH') AND isset($_SESSION['username']) AND ($_SESSION['username']!="" || $_SESSION['username']!=null  )) OR exit('Access denied!');
 
