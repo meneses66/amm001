b927df92abe1adc8f811283042645c34b998f80a
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-14 19:04:43 +0000

    20250414

--- ammpet/classes/views/home/home.view.php
+++ ammpet/classes/views/home/home.view.php
@@ -13,7 +13,7 @@
     <?php include ROOTPATH_CLASSES . "../views/head.view.php";?>
     </head>
     <body>
-        <input id="user_permissions" type="text" value="<?php echo $_SESSION['permissions']; ?>">
+        <input id="user_permissions" type="text" value="xxxxxx">
         <div class="d-flex" id="wrapper">
             <!-- Sidebar-->
             <div class="border-end bg-white" id="sidebar-wrapper">
