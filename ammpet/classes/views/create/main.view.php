a11049ed59084d0b3c02075e4e25fc326d0f42b0
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-15 13:02:42 +0000

    20250415

--- ammpet/classes/views/create/main.view.php
+++ ammpet/classes/views/create/main.view.php
@@ -10,15 +10,17 @@
     <hr class="my-1">
     <br><br><br><br>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Client/_new";?>" class="btn btn-primary btn-lg m-1 btn-block" id="client_new"><i class="fas fa-user-plus"></i>&nbsp;Novo Cliente</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Supplier/_new";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-id-badge"></i>&nbsp;Novo Fornecedor</a></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Product/_new";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-barcode"></i>&nbsp;Novo Produto</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Service/_new";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-cut"></i>&nbsp;Novo Serviço</a></div>
-    </div>
-    <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Breed/_new";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-paw"></i>&nbsp;Nova Raça</a></div>
-        <div class="col-sm-6"></div>
+        <div class="row">
+                <div class="col-sm-6"><?((check_permission($_SESSION['username'],"client_add"))? "<a href=\"" . ROOT . "/Client/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-user-plus\"></i>&nbsp;Novo Cliente</a>" :"");?></div>
+                <div class=\"col-sm-6\"><a href=\"" . ROOT . "/Supplier/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-id-badge\"></i>&nbsp;Novo Fornecedor</a></div>
+                </div>
+                <div class=\"row\">
+                    <div class=\"col-sm-6\"><a href=\"" . ROOT . "/Product/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-barcode\"></i>&nbsp;Novo Produto</a></div>
+                    <div class=\"col-sm-6\"><a href=\"" . ROOT . "/Service/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-cut\"></i>&nbsp;Novo Serviço</a></div>
+                </div>
+                <div class=\"row\">
+                    <div class=\"col-sm-6\"><a href=\"" . ROOT . "/Breed/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-paw\"></i>&nbsp;Nova Raça</a></div>
+                    <div class=\"col-sm-6\"></div>
+        </div>;
     </div>
 </div>
