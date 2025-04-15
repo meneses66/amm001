f648b086695d0c2f0d6f07d5274c33e59c63f165
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-15 18:58:16 +0000

    20250415

--- ammpet/classes/views/report/main.view.php
+++ ammpet/classes/views/report/main.view.php
@@ -10,16 +10,16 @@
     <hr class="my-1">
     <br><br><br><br>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/cobrancas";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Cobranças</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/vencimento_pacotes";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Vencimento Pacotes</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"cobrancas_view"))? print_r("<a href=\"". ROOT."/Report/cobrancas\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Cobranças</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"vencimento_pacotes_view"))? print_r("<a href=\"". ROOT."/Report/vencimento_pacotes\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Vencimento Pacotes</a>") : print_r(""))?></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/vencimento_vacinas";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Vencimento Vacinas</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/tosador";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Tosador</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"vencimento_vacinas_view"))? print_r("<a href=\"". ROOT."/Report/vencimento_vacinas\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Vencimento Vacinas</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"tosador_view"))? print_r("<a href=\"". ROOT."/Report/tosador\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Tosador</a>") : print_r(""))?></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/veterinaria";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Veterinária</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Report/comissoes";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Comissões</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"vet_view"))? print_r("<a href=\"". ROOT."/Report/veterinaria\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Veterinária</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"comis_view"))? print_r("<a href=\"" . ROOT . "/Report/comissoes\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-list-alt\"></i>&nbsp;Comissões</a>") : print_r(""))?></div>
     </div>
     <br>
 </div>
