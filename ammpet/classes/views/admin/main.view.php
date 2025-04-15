a1a2d51134280a4be8481ec24fef6d9dc74f6cfe
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-15 14:05:49 +0000

    20250415

--- ammpet/classes/views/admin/main.view.php
+++ ammpet/classes/views/admin/main.view.php
@@ -10,19 +10,22 @@
     <hr class="my-1">
     <br><br><br><br>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Daily_Accounting/_list";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-cash-register"></i>&nbsp;Caixa</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Payments/_list";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-file-invoice-dollar"></i>&nbsp;Custos</a></div>
+    
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"client_add"))? print_r("<a href=\"" . ROOT . "/Client/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-user-plus\"></i>&nbsp;Novo Cliente</a>") : print_r(""))?></div>
+
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"cash_register_view"))? print_r("<a href=\"" . ROOT ."/Daily_Accounting/_list\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-cash-register\"></i>&nbsp;Caixa</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"cost_view"))? print_r("<a href=\"" . ROOT ."/Payments/_list\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-file-invoice-dollar\"></i>&nbsp;Custos</a>") : print_r(""))?></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Salary/_list";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-hand-holding-usd"></i>&nbsp;Salários</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Params/_list";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-indent"></i>&nbsp;Parâmetros Sistema</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"salary_view"))? print_r("<a href=\"" . ROOT ."/Salary/_list\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-hand-holding-usd\"></i>&nbsp;Salários</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"params_view"))? print_r("<a href=\"" . ROOT ."/Params/_list\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-indent\"></i>&nbsp;Parâmetros Sistema</a>") : print_r(""))?></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Pre_Closing/pre_closing";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-percent"></i>&nbsp;Pré Fechamento</a></div>
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Month_Closing/month_closing";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-chart-pie"></i>&nbsp;Fechamento Mês</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"pre_closing_view"))? print_r("<a href=\"" . ROOT ."/Pre_Closing/pre_closing\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-percent\"></i>&nbsp;Pré Fechamento</a>") : print_r(""))?></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"month_closing_view"))? print_r("<a href=\"" . ROOT ."/Month_Closing/month_closing\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-chart-pie\"></i>&nbsp;Fechamento Mês</a>") : print_r(""))?></div>
     </div>
     <div class="row">
-        <div class="col-sm-6"><a href="<?php echo ROOT."/Results/_list";?>" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-chart-line"></i>&nbsp;Resultados</a></div>
+        <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"client_add"))? print_r("<a href=\"" . ROOT ."/Results/_list\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-chart-line\"></i>&nbsp;Resultados</a>") : print_r(""))?></div>
         <div class="col-sm-6"></div>
     </div>
     <br>
--- ammpet/classes/views/create/main.view.php
+++ ammpet/classes/views/create/main.view.php
@@ -11,16 +11,16 @@
     <br><br><br><br>
     <div class="row">
         <div class="row">
-            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"client_add"))? print_r("<a href=\"" . ROOT . "/Client/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-user-plus\"></i>&nbsp;Novo Cliente</a>") : print_r("Novo Cliente - Sem Acesso"))?></div>
-            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"supplier_add"))? print_r("<a href=\"" . ROOT . "/Supplier/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-id-badge\"></i>&nbsp;Novo Fornecedor</a>") : print_r("Novo Fornecedor - Sem Acesso"))?></div>
+            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"client_add"))? print_r("<a href=\"" . ROOT . "/Client/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-user-plus\"></i>&nbsp;Novo Cliente</a>") : print_r(""))?></div>
+            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"supplier_add"))? print_r("<a href=\"" . ROOT . "/Supplier/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"far fa-id-badge\"></i>&nbsp;Novo Fornecedor</a>") : print_r(""))?></div>
         </div>
         <div class="row">
-            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"product_add"))? print_r("<a href=\"" . ROOT . "/Product/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-barcode\"></i>&nbsp;Novo Produto</a>") : print_r("Novo Produto - Sem Acesso"))?></div>
-            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"service_add"))? print_r("<a href=\"" . ROOT . "/Service/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-cut\"></i>&nbsp;Novo Serviço</a>") : print_r("Novo Serviço - Sem Acesso"))?></div>
+            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"product_add"))? print_r("<a href=\"" . ROOT . "/Product/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-barcode\"></i>&nbsp;Novo Produto</a>") : print_r(""))?></div>
+            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"service_add"))? print_r("<a href=\"" . ROOT . "/Service/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-cut\"></i>&nbsp;Novo Serviço</a>") : print_r(""))?></div>
         </div>
         <div class="row">
-            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"breed_add"))? print_r("<a href=\"" . ROOT . "/Breed/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-paw\"></i>&nbsp;Nova Raça</a>") : print_r("Nova Raça - Sem Acesso"))?></div>
+            <div class="col-sm-6"><?php ((check_permission($_SESSION['username'],"breed_add"))? print_r("<a href=\"" . ROOT . "/Breed/_new\" class=\"btn btn-primary btn-lg m-1 btn-block\"><i class=\"fas fa-paw\"></i>&nbsp;Nova Raça</a>") : print_r(""))?></div>
             <div class="col-sm-6"></div>
-        </div>;
+        </div>
     </div>
 </div>
