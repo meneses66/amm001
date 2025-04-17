08d1b4fa6dd8b9c240c4f32207e4c590b23a5f74
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-17 15:16:23 +0000

    20250417

--- ammpet/classes/core/functions.php
+++ ammpet/classes/core/functions.php
@@ -326,12 +326,12 @@ function check_permission($username, $permission_to_check){
 
 function wh_log($log_msg)
 {
-    $log_filename = "log_amm";
+    $log_filename = "log";
     if (!file_exists($log_filename)) 
     {
         // create directory/folder uploads.
         mkdir($log_filename, 0777, true);
     }
-    $log_file_data = $log_filename.'/log_' . date('Y-M-d') . '.log';
+    $log_file_data = $log_filename.'/log_amm_' . date('Y-M-d') . '.log';
     file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
 } 
