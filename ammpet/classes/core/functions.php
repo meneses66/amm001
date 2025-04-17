ea5096f85817c07a0b67cc55dbae2131c0dc2add
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-17 15:34:58 +0000

    20250417

--- ammpet/classes/core/functions.php
+++ ammpet/classes/core/functions.php
@@ -326,7 +326,7 @@ function check_permission($username, $permission_to_check){
 
 function wh_log($log_msg)
 {
-    $log_filename = $_SERVER['DOCUMENT_ROOT']."/log";
+    $log_filename = ROOTPATH_CLASSES."/log";
     if (!file_exists($log_filename)) 
     {
         // create directory/folder uploads.
