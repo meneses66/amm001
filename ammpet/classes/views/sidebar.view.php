bc11a29f5c7e29ab929880360518380bb11d4620
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-17 16:08:44 +0000

    20250417

--- ammpet/classes/views/sidebar.view.php
+++ ammpet/classes/views/sidebar.view.php
@@ -1,8 +1,5 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 <div class="sidebar-heading border-bottom bg-light">ABC System</div>
-<div><? echo ROOT ?></div>
-<div><? echo ROOTPATH ?></div>
-<div><? echo ROOTPATH_CLASSES ?></div>
   <div class="list-group list-group-flush">
       <a class="list-group-item list-group-item-action <?php if(null !== URL_0) {if(URL_0=='Home' && URL_1=="") echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home";?>"><i class="fa fa-home"></i>&nbsp;Home</a>
       <a class="list-group-item list-group-item-action <?php if(null!== URL_0 && null !== URL_1) {if(URL_0=='Home' && URL_1=='create') echo 'active';}?> list-group-item-primary p-3" href="<?php echo ROOT."/Home/create";?>"><i class="fas fa-folder-plus"></i>&nbsp;Cadastros</a>
