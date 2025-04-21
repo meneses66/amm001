1aedba8f2ff24e996887ae7d0edfe194dcb020f7
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-21 19:56:06 +0000

    20250421

--- ammpet/public/assets/js/load_salary_form.js
+++ ammpet/public/assets/js/load_salary_form.js
@@ -74,12 +74,14 @@ $(document).ready(function(){
     }
     
     function update_salary(data_form, method_update){
-        var data_form2 = splitSerializedEncoded(data_form);
+        //var data_form2 = splitSerializedEncoded(data_form);
+        var data_form3 = splitUrlEncoded(data_form);
         console.log("INSERT ACCESSED:"+data_form2);
         $.ajax({
             url: "/ammpet/public/Ajax_call",
             type: "POST",
-            data: {class:"Salary", method:method_update, data_form2},
+            data: {class:"Salary", method:method_update, Created_By:data_form3[Created_By],Updated_by:data_form3[Updated_by],Id_Employee:data_form3[Id_Employee],Ref_Date:data_form3[Ref_Date],Salary_Item_Type:data_form3[Salary_Item_Type],Salary_Item_Value:data_form3[Salary_Item_Value],Salary_Item_Status:data_form3[Salary_Item_Status],Original_Value:data_form3[Original_Value],Postponed_Value:data_form3[Postponed_Value],Salary_Item_Description:data_form3[Salary_Item_Description]"}
+            },
             success: function(response){
             }
         });
