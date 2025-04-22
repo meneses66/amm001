17422ed425a3d6953a2b93a372faeeccfc938e5a
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-22 22:53:25 +0000

    20250422

--- ammpet/classes/views/home/main.view.php
+++ ammpet/classes/views/home/main.view.php
@@ -11,11 +11,11 @@
     <br><br><br><br>
     <div class="row">
         <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"client_view")) {echo '<a href="' . ROOT . '/Client/_list" class="btn btn-primary btn-lg m-1 btn-block"><i class="fas fa-user"></i>&nbsp;Clientes</a>';}?></div>
-        <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"orderx_view")) {echo '<a href="'.  ROOT . '/OrderX/_list" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Vendas</a>';}?></div>
+        <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"orderx_view")) {echo '<a href="'.  ROOT . '/Orderx/_list" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-list-alt"></i>&nbsp;Vendas</a>';}?></div>
     </div>
     <div class="row">
         <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"agenda_view")) {echo '<a href="'. ROOT . '/Agenda/week_view" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-calendar-alt"></i>&nbsp;Agenda</a>';}?></div>
-        <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"orderx_add")) {echo '<a href="'. ROOT . '/OrderX/_new" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-money-bill-alt"></i>&nbsp;Nova Venda</a>';}?></div>
+        <div class="col-sm-6"><?php if(check_permission($_SESSION['username'],"orderx_add")) {echo '<a href="'. ROOT . '/Orderx/_new" class="btn btn-primary btn-lg m-1 btn-block"><i class="far fa-money-bill-alt"></i>&nbsp;Nova Venda</a>';}?></div>
     </div>
     <br>
 </div>
