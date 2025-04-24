187143397403feb5129db9bb5d9c9e06a766fcca
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-24 23:16:48 +0000

    20250424

--- ammpet/classes/controllers/Breed.php
+++ ammpet/classes/controllers/Breed.php
@@ -253,7 +253,7 @@ class Breed {
                             <td>'.$row->NAME.'</td>
                             <td>'.$row->TYPE.'</td>
                             <td>
-                                '.(($breed_edit_check) ? "<a href=\"" . ROOT . "/$this->UCF_object/_update?id=$row->ID\" title=\"Edit\" class=\"text-primary updateBtn\" id=" . $row->ID . "><i class=\"fas fa-edit\"></i></a>&nbsp;&nbsp" : "").'
+                                '.(($breed_edit_check) ? "<a href=\"" . ROOT . "/$this->UCF_object/_new?id=$row->ID\" title=\"Edit\" class=\"text-primary updateBtn\" id=" . $row->ID . "><i class=\"fas fa-edit\"></i></a>&nbsp;&nbsp" : "").'
                                 '.(($breed_delete_check) ? "<a href=\"" . ROOT . "/$this->UCF_object/_delete?id=$row->ID\" title=\"Edit\" class=\"text-danger deleteBtn\" id=" . $row->ID . "><i class=\"fas fa-eraser\"></i></a>" : "").'
                             </td></tr>';
             }
