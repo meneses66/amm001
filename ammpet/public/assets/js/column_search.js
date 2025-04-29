7eb0e2989931c2034ffc9d6ccd5cda7edba02850
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-29 13:17:04 +0000

    20250429

--- ammpet/public/assets/js/column_search.js
+++ ammpet/public/assets/js/column_search.js
@@ -1,24 +1,2 @@
 $(document).ready(function(){
-    $('#_table thead th').each( function () {
-        var title = $('#_table tfoot th').eq( $(this).index() ).text();
-        if(title!=""){
-            $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-      }
-    } );
-  
-    // DataTable
-    var table = document.getElementById('_table');
-
-    // Apply the search
-    table.columns().eq( 0 ).each( function ( colIdx ) {
-        if( !table.settings()[0].aoColumns[colIdx].bSearchable ){
-        table.column( colIdx ).header().innerHTML=table.column( colIdx ).footer().innerHTML;
-    }
-        $( 'input', table.column( colIdx ).header() ).on( 'keyup change', function () {
-            table
-                .column( colIdx )
-                .search( this.value )
-                .draw();
-        } );
-    } );
 } );
