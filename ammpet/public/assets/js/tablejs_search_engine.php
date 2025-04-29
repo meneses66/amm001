f16bb676cbca68a9fdf7305089a8cce00cd6fcb6
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-29 13:27:00 +0000

    20250429

--- ammpet/public/assets/js/tablejs_search_engine.php
+++ ammpet/public/assets/js/tablejs_search_engine.php
@@ -35,27 +35,7 @@ $output = '<script type="text/javascript">
                                         render: DataTable.render.datetime(\'YYYY-MM-DD\')
                                     }
                                             ],
-                                "order": [[ 1, "desc" ]],
-                                initComplete: function () {
-                                                    this.api()
-                                                        .columns()
-                                                        .every(function () {
-                                                            let column = this;
-                                                            let title = column.footer().textContent;
-                                            
-                                                            // Create input element
-                                                            let input = document.createElement(\'input\');
-                                                            input.placeholder = title;
-                                                            column.footer().replaceChildren(input);
-                                            
-                                                            // Event listener for user input
-                                                            input.addEventListener(\'keyup\', () => {
-                                                                if (column.search() !== this.value) {
-                                                                    column.search(input.value).draw();
-                                                                }
-                                                            });
-                                                        });
-                                }
+                                "order": [[ 1, "desc" ]]
 
                             });
                         }
