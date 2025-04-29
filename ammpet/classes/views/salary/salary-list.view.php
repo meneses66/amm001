58933328815222b5c9c3479f4c9051ba363c88e6
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-29 19:05:57 +0000

    20250429

--- ammpet/classes/views/salary/salary-list.view.php
+++ ammpet/classes/views/salary/salary-list.view.php
@@ -29,9 +29,8 @@ $_SESSION['LAST_ACTIVE']=time();?>
         <script src="<?php echo ROOT;?>/assets/js/scripts.js"></script>
 
         <!-- jQuery library -->
-        <script src="<?php echo ROOT;?>/assets/js/jquery-3.7.1.js"></script>
-        <!-- <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script> -->
-
+        <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+        
         <!-- Latest compiled JavaScript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
         
