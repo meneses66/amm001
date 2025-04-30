ee00340328a89d5af6f33541b28692f78a50c399
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-04-30 13:29:43 +0000

    20250430

--- ammpet/classes/views/salary/salary-list.view.php
+++ ammpet/classes/views/salary/salary-list.view.php
@@ -49,8 +49,8 @@ $_SESSION['LAST_ACTIVE']=time();?>
        <!-- FORMAT TABLE JS-->
        <script src="<?php echo ROOT;?>/assets/js/format_table.js"></script>
 
-        <!-- Table Pagination, Search, Refresh and Ajax Call-->
-        <?php include removeFromEnd(ROOTPATH_CLASSES,"classes/core/") . "public/assets/js/tablejs_search_engine.php";?>
+       <!-- Table Pagination, Search, Refresh and Ajax Call JS-->
+       <script src="<?php echo ROOT;?>/assets/js/table_search_engine.js"></script>
 
         <!-- Table Column Date Formatting-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
