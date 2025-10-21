d1ffe4d27d9a492c4cfba19f91225925b85c210b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-10-21 02:27:10 +0000

    Codex_004

--- ammpet/classes/core/config.php
+++ ammpet/classes/core/config.php
@@ -7,8 +7,8 @@ spl_autoload_register(function($fqcn){
     // Base path for classes directory using ROOTPATH_CLASSES (classes/core/) -> up to classes/
     $base = removeFromEnd(ROOTPATH_CLASSES, 'core/');
 
-    // Normalize namespace and class
-    $parts = explode('\\\\', $fqcn);
+    // Normalize namespace and class (split by single backslash)
+    $parts = explode('\\', $fqcn);
     $top = $parts[0] ?? '';
     $class = end($parts);
 
