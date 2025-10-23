59e9fba4573e66da490d6d5a169d16908ddabc5b
Alex Melo <43595648+meneses66@users.noreply.github.com>
2025-10-23 21:41:19 +0000

    Caodex_020

--- ammpet/classes/views/orderx/orderx-new.view.php
+++ ammpet/classes/views/orderx/orderx-new.view.php
@@ -44,8 +44,10 @@ $_SESSION['LAST_ACTIVE']=time();?>
         <!-- Sweet Alert -->
         <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+        <!-- Table formatting (search/paging) -->
+        <script src="<?php echo ROOT;?>/assets/js/format_table_search.js"></script>
         <!-- Table Pagination, Search, Refresh and Ajax Call-->
-        <?php include removeFromEnd(ROOTPATH_CLASSES,"classes/core/") . "public/assets/js/tablejs_no_buttons.php";?>
+        <?php include removeFromEnd(ROOTPATH_CLASSES,"classes/core/") . "public/assets/js/tablejs_v2.php";?>
 
         <!-- Table Column Date Formatting-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
